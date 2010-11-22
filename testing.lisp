a852b06e2ce40e2f1a6c67f51f3ccf57ae3a08d3
mikel evins <mevins@me.com>
2010-11-22 02:10:09 -0700

    added some more test data; figure out how to make multi-column-list-panel draw alternating background colors

--- testing.lisp
+++ testing.lisp
@@ -8,11 +8,25 @@
    (setf $zips (read-csv (namestring (cl-user::path "test-data/zipcode.csv"))))
    'done))
 
+(time
+ (progn
+   (setf $logms (read-csv (namestring (cl-user::path "test-data/logmessages.csv")) 
+                          :first-row-headers? nil))
+   'done))
+
 (time 
  (progn
      (setf $doc
       (make-instance 'document
-                     :presentation (make-instance 'presentation :model $zips)))))
+                     :presentation (make-instance 'presentation :model $logms)))))
+
+;;; yay! this works:
+(objc:invoke
+ (slot-value 
+  (slot-value (first (layout-description (table-rows (window $doc))))
+              'capi-internals::representation)
+  'capi-cocoa-library::main-view)
+ "setUsesAlternatingRowBackgroundColors:" t)
 
 (time 
  (progn
