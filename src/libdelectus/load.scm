36d14eb878be1c370976a650ad89cec269aa30c2
mikel evins <mevins@me.com>
2010-12-05 21:37:29 -0700

    implementing the delectus data engine as a Gambit library

---
+++ src/libdelectus/load.scm
@@ -0,0 +1,24 @@
+;;;; ***********************************************************************
+;;;; FILE IDENTIFICATION
+;;;;
+;;;; Name:          load.scm
+;;;; Project:       Delectus/libdelectus
+;;;; Purpose:       loader for repl testing
+;;;; Author:        mikel evins
+;;;;
+;;;; ***********************************************************************
+
+;;; ----------------------------------------------------------------------
+;;; About
+;;; ----------------------------------------------------------------------
+;;; libdelectus packages the Delectus data engine as a library
+
+;;; modify if the Delectus sources are at another pathname
+
+(define $root "/Users/mikel/Projects/delectus/delectus")
+
+;;; Scheme files to load for interactive testing
+;;; ----------------------------------------------------------------------
+
+(define $load-files
+  '())
