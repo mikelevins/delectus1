359b9463466e7ad2a778479c913785caacc0492e
mikel evins <mevins@me.com>
2010-12-05 21:47:29 -0700

    tidied up loadfile

--- src/libdelectus/load.scm
+++ src/libdelectus/load.scm
@@ -15,10 +15,19 @@
 
 ;;; modify if the Delectus sources are at another pathname
 
-(define $root "/Users/mikel/Projects/delectus/delectus")
+(define $root "/Users/mikel/Projects/delectus/src/libdelectus/")
 
 ;;; Scheme files to load for interactive testing
 ;;; ----------------------------------------------------------------------
 
 (define $load-files
-  '())
+  '("model/delectus.scm"
+    ))
+
+(define (load-delectus)
+  (for-each (lambda (f)
+              (let ((p (string-append $root f)))
+                (load p)))
+            $load-files))
+
+;;; (load-delectus)
