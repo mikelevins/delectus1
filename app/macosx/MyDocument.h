bcb346d21f4b833ac2a8061cb6a1279d0eef7ac4
mikel evins <mevins@me.com>
2010-12-22 04:42:26 -0600

    experimenting with building the gambit-based engine as a C library

---
+++ app/macosx/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Delectus
+//
+//  Created by mikel on 12/21/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
