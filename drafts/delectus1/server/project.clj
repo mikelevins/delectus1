d507cc473d9b1e9718b3abe99725de5e990eb62a
mikel evins <mevins@me.com>
2011-02-21 14:51:07 -0600

    reorganized to make Delectus 1 the main development branch

--- drafts/delectus1/server/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject server "1.0.0-SNAPSHOT"
-  :description "Delectus web services"
-  :main com.bywicket.delectus.server.core
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
