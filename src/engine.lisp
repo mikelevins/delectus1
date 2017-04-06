5494a101b3cb2dc15e1b43f14c1ed4cbe38d69d7
mikel evins <mevins@me.com>
2017-04-05 20:46:36 -0500

    working on an API for creating and interacting with store files

--- src/engine.lisp
+++ src/engine.lisp
@@ -19,13 +19,16 @@
 ;;; ---------------------------------------------------------------------
 (defclass store ()
   (;; a reference to the database in which data are stored
-   (db-handle :reader db-handle :initform nil :initarg :db-handle)
-   ;; the pathname of the backing file, or ":memory:" if it's an in-memory db
    (db-path :reader db-path :initform nil :initarg :db-path)))
 
+;;; TODO: currently it squeals if the file exists already
+;;;       what we want to do is peek at the file to see if it's
+;;;       a delectus2 file; if it is, just go ahead and return the
+;;;       store; if it isn't, then squeal
 (defmethod initialize-instance :after ((store store) &rest initargs &key &allow-other-keys)
-  (if (db-path store)
-      ;; create the database file
-      ()
-    ;; create an in-memory database
-    ()))
+  (assert (db-path store)() "Creating a store requires that you supply a pathname for a store file")
+  (with-open-database (db (db-path store))
+    (with-transaction db
+      (execute-non-query db "create table delectus (format_version integer)")
+      (execute-non-query db "insert into delectus (format_version) values (?)" +delectus-format-version+)
+      (execute-non-query db "create table contents (rowid integer primary key) without rowid"))))
