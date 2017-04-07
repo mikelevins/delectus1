01773ea2f0409b97d5bab8d82eb23bc9dd1fc8d8
mikel evins <mevins@me.com>
2017-04-06 19:55:02 -0500

    added code to test whether a path names a valid Delectus file

--- src/engine.lisp
+++ src/engine.lisp
@@ -21,14 +21,52 @@
   (;; a reference to the database in which data are stored
    (db-path :reader db-path :initform nil :initarg :db-path)))
 
-;;; TODO: currently it squeals if the file exists already
-;;;       what we want to do is peek at the file to see if it's
-;;;       a delectus2 file; if it is, just go ahead and return the
-;;;       store; if it isn't, then squeal
-(defmethod initialize-instance :after ((store store) &rest initargs &key &allow-other-keys)
-  (assert (db-path store)() "Creating a store requires that you supply a pathname for a store file")
-  (with-open-database (db (db-path store))
+(defmethod create-delectus-file ((path pathname))
+  (with-open-database (db path)
     (with-transaction db
       (execute-non-query db "create table delectus (format_version integer)")
       (execute-non-query db "insert into delectus (format_version) values (?)" +delectus-format-version+)
       (execute-non-query db "create table contents (rowid integer primary key) without rowid"))))
+
+(defmethod create-delectus-file ((path string))
+  (create-delectus-file (pathname path)))
+
+;;; TODO: check whether the path names a valid delectus file
+;;; - it exists
+;;; - it's a file
+;;; - it's a SQLite file
+;;; - it contains the proper Delectus tables
+(defmethod valid-delectus-file? ((path pathname))
+  (and (probe-file path)
+       (uiop/pathname:file-pathname-p path)
+       (handler-case (with-open-database (db path)
+                       (execute-non-query db "pragma schema_version")
+                       t)
+         ;; the file was not a SQLite database
+         (sqlite-error (err) (warn "Not a Delectus store file (not a database): ~S" path) nil)
+         ;; some other error occurred when reading the file
+         (error (err) (warn "Unable to open file: ~S" path) nil))
+       ;; if we reach this code then the path names a SQLite database file
+       ;; check to see whether it has the required Delectus tables:
+       ;; 1. table: delectus
+       (handler-case (with-open-database (db path)
+                       (execute-non-query db "select * from delectus")
+                       t)
+         (sqlite-error (err) (warn "Not a Delectus store file (missing Delectus table): ~S" path) nil))
+       ;; 2. table: contents
+       (handler-case (with-open-database (db path)
+                       (execute-non-query db "select * from contents limit 1")
+                       t)
+         (sqlite-error (err) (warn "Not a Delectus store file (missing contents table): ~S" path) nil))))
+
+(defmethod valid-delectus-file? ((path string))
+  (valid-delectus-file? (pathname path)))
+
+(defmethod initialize-instance :after ((store store) &rest initargs &key &allow-other-keys)
+  (let ((store-path (db-path store)))
+    (if (probe-file store-path)
+        (unless (valid-delectus-file? store-path)
+          (setf (slot-value store 'db-path) nil)
+          (error "File is not a valid Delectus store: ~A" store-path))
+      (create-delectus-file store-path))))
+
