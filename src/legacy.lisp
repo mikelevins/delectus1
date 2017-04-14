ce019d5e52105b877efd6c2db833acc5b0085c2d
mikel <mikel@jupiter.attlocal.net>
2017-04-13 22:01:55 -0500

    initial sexp->sqlite converter (BUG: it upcases all text)

--- src/legacy.lisp
+++ src/legacy.lisp
@@ -30,12 +30,12 @@
 ;;; The legacy code reads a file in this format and converts the data
 ;;; to Delectus 2 data suitable for storage in a SQLite file.
 
-(defmethod read-delectus-sexp-file ((path pathname))
+(defmethod read-delectus-sexp-file0 ((path pathname))
   "Read a .sexp file (a Delectus 1.x file in s-expression format)"
   (let* ((filetype (pathname-type path)))
     (assert (equal "sexp" filetype)()
-      "Expected a text file of type \".sexp\" but found ~S"
-      (format nil "~A.~A" (pathname-name path)(pathname-type path)))
+            "Expected a text file of type \".sexp\" but found ~S"
+            (format nil "~A.~A" (pathname-name path)(pathname-type path)))
     (with-open-file (in path)
       (let ((column-sentinel nil)
             (columns nil)
@@ -61,3 +61,58 @@
   (read-delectus-sexp-file (pathname path)))
 
 ;;; (defparameter $data (read-delectus-sexp-file "/Users/mikel/Desktop/raw-delectus-data.sexp"))
+
+(defmethod convert-delectus-sexp-file ((path pathname) &optional (outpath nil))
+  (let* ((in-basename (pathname-name path))
+         (outpath (or outpath
+                      (make-pathname :directory (pathname-directory path)
+                                     :name (pathname-name path)
+                                     :type "delectus2"))))
+    (with-open-file (in path :direction :input)
+      (let ((column-sentinel nil))
+        (setf column-sentinel (read in))
+        (assert (eql :COLUMNS column-sentinel)() "File format error: Expected :COLUMNS but found ~S"
+                column-sentinel)
+        ;; columns format:
+        ;; (:LABEL "Title" :DELETED NIL :LABEL "Star" :DELETED NIL :LABEL "Costar" :DELETED NIL ...)
+        (let ((columns (read in))
+              (rows-sentinel (read in)))
+          (assert (eql :ROWS rows-sentinel)() "File format error: Expected :ROWS but found ~S"
+                  rows-sentinel)
+          (let* ((stripped (loop for x in (cdr columns) by #'cddr collect x))
+                 (pairs (loop for tail on stripped by #'cddr
+                           collect (cons (car tail)(cadr tail))))
+                 (deleted-pairs (remove-if-not #'(lambda (pair)(cdr pair))
+                                               pairs))
+                 (column-labels (cons "deleted" (mapcar #'car pairs)))
+                 (deleted-labels (mapcar #'car deleted-pairs))
+                 (create-contents-sql (format nil "create table contents (~{~a~^, ~})"
+                                              column-labels)))
+            (with-open-database (db outpath)
+              (with-transaction db
+                ;; table: delectus - identifies format version
+                (execute-non-query db "create table delectus (format_version integer)")
+                (execute-non-query db "insert into delectus (format_version) values (?)" +delectus-format-version+)
+                ;; table: contents - stores document data
+                (execute-non-query db create-contents-sql)
+                ;; table: column_order - stores the user-defined column order
+                (execute-non-query db "create table column_order (column_name string)")
+                (dolist (lbl column-labels)
+                  (execute-non-query db "insert into column_order (column_name) values (?)" lbl))
+                ;; table: deleted_columns - stores labels of columns that are present but marked deleted
+                (execute-non-query db "create table deleted_columns (column_name string)")
+                (dolist (lbl deleted-labels)
+                  (execute-non-query db "insert into deleted_columns (column_name) values (?)" lbl))
+                ;; read and insert rows
+                (loop for row = (read in nil nil) then (read in nil nil)
+                   while row
+                   do (let* ((fields (mapcar #'(lambda (f)
+                                                 (cond ((not f) "0")
+                                                       ((eql f t) "1")
+                                                       (t f)))
+                                             (loop for x in (cdr row) by #'cddr collect x)))
+                             (insert-sql (format nil "insert into contents values (~{~s~^, ~})" fields)))
+                        (execute-non-query db insert-sql)))))))))))
+
+(defmethod convert-delectus-sexp-file ((path string) &optional (outpath nil))
+  (convert-delectus-sexp-file (pathname path) outpath))
