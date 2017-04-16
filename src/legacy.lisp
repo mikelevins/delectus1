4688a1857f1a1b9aab8edb119073e6612ab39bae
mikel <mikel@jupiter.attlocal.net>
2017-04-16 10:22:58 -0500

    removed obsolete read-delectus-sexp-file

--- src/legacy.lisp
+++ src/legacy.lisp
@@ -30,38 +30,6 @@
 ;;; The legacy code reads a file in this format and converts the data
 ;;; to Delectus 2 data suitable for storage in a SQLite file.
 
-(defmethod read-delectus-sexp-file0 ((path pathname))
-  "Read a .sexp file (a Delectus 1.x file in s-expression format)"
-  (let* ((filetype (pathname-type path)))
-    (assert (equal "sexp" filetype)()
-            "Expected a text file of type \".sexp\" but found ~S"
-            (format nil "~A.~A" (pathname-name path)(pathname-type path)))
-    (with-open-file (in path)
-      (let ((column-sentinel nil)
-            (columns nil)
-            (rows nil)
-            (row-sentinel nil))
-        ;; read columns
-        ;; read rows
-        ;; TODO: if the file is really big then it might not
-        ;;       be a good idea to read all the rows at once
-        ;;       rewrite the reader to read a row at a time
-        ;;       in an SQLite transaction, adding each row to
-        ;;       the store as we read it
-        (setf column-sentinel (read in))
-        (assert (eql :COLUMNS column-sentinel)() "Expected :COLUMNS but found ~S" column-sentinel)
-        (setf columns (read in))
-        (setf row-sentinel (read in))
-        (assert (eql :ROWS row-sentinel)() "Expected :ROWS but found ~S" row-sentinel)
-        (setf rows (loop for row = (read in nil) then (read in nil) while row collect row))
-        (list :columns columns :rows rows)))))
-
-(defmethod read-delectus-sexp-file ((path string))
-  "Read a .sexp file (a Delectus 1.x file in s-expression format)"
-  (read-delectus-sexp-file (pathname path)))
-
-;;; (defparameter $data (read-delectus-sexp-file "/Users/mikel/Desktop/raw-delectus-data.sexp"))
-
 ;;; convert-delectus-sexp-file ((path pathname) &optional (outpath nil))
 ;;; ---------------------------------------------------------------------
 (defmethod convert-delectus-sexp-file ((path pathname) &optional (outpath nil))
