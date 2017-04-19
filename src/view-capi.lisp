24622c1430656cfddc07bca4f6b081790d9b4499
mikel <mikel@jupiter.attlocal.net>
2017-04-19 15:17:46 -0500

    started work on integrating capi ui with the updated backend code

--- src/view-capi.lisp
+++ src/view-capi.lisp
@@ -14,4 +14,29 @@
 ;;; view classes
 ;;; ---------------------------------------------------------------------
 
+(define-interface delectus-ui ()
+  ;; -- slots ---------------------------------------------
+  ((document-path :accessor document-path :initform nil :initarg :document-path))
 
+  ;; -- panes ---------------------------------------------
+  (:panes
+   (contents-pane multi-column-list-panel :reader contents-pane
+                  :alternating-background t
+                  :columns (compute-column-descriptions interface)
+                  :items `(("Home Sweet Home" "Joe" "blah blah")
+                           ("Another Title" "A. Nother Author" "various gibberish")
+                           ("Duke" "L. Lington" "she doob e")))
+   (count-pane title-pane :reader count-pane :text (format nil "some items")))
+  
+  ;; -- layouts ---------------------------------------------
+  (:layouts
+   (main-layout column-layout '(contents-pane count-pane)
+                :reader main-layout :border 4))
+
+  
+  ;; -- defaults ---------------------------------------------
+  (:default-initargs :layout 'main-layout
+   :width 800 :height 600))
+
+(defmethod compute-column-descriptions ((ui delectus-ui))
+  `((:title "Title" :default-width 96)(:title "Byline" :default-width 96)(:title "Credits" :default-width 96)))
