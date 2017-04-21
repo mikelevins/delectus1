afadf8ea8d58a6bf11c0ebd618274020f43cf6d5
mikel <mikel@jupiter.attlocal.net>
2017-04-20 20:07:12 -0500

    implemenented sorting in view-capi

--- src/view-capi.lisp
+++ src/view-capi.lisp
@@ -22,9 +22,12 @@
   (:panes
    (contents-pane multi-column-list-panel :reader contents-pane
                   :alternating-background t
+                  :header-args '(:selection-callback :sort)
+                  :sort-descriptions (compute-column-sort-descriptions interface)
                   :columns (compute-column-descriptions document)
                   :items (compute-visible-rows document))
-   (count-pane title-pane :reader count-pane :text (format nil "some items")))
+   (count-pane title-pane :reader count-pane 
+               :text (format nil "~A items" (length (compute-visible-rows (document interface))))))
   
   ;; -- layouts ---------------------------------------------
   (:layouts
@@ -45,12 +48,20 @@
     (mapcar (lambda (lbl) `(:title ,lbl :default-width 96))
             column-labels)))
 
-
-
 (defmethod compute-visible-rows ((document null)) nil)
 
 (defmethod compute-visible-rows ((document document))
   (visible-rows document
                 (visible-column-labels document)))
 
+(defun element-getter (n)
+  #'(lambda (it)(elt it n)))
+
+(defmethod compute-column-sort-descriptions ((ui delectus-ui))
+  (let* ((column-labels (visible-column-labels (document ui))))
+    (loop for i from 0 below (length column-labels)
+          collect (capi:make-sorting-description :type (elt column-labels i)
+                                                 :key (element-getter i)
+                                                 :sort 'string-lessp
+                                                 :reverse-sort 'string-greaterp))))
 
