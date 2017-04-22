9bd8680562beb6faa454fcfb5494b8ef1d9e37e0
mikel evins <mevins@me.com>
2017-04-21 19:20:59 -0500

    adding widgets to the main ui

--- src/view-capi.lisp
+++ src/view-capi.lisp
@@ -35,16 +35,26 @@
                   :columns (compute-column-descriptions document)
                   :items (compute-visible-rows document))
    (count-pane title-pane :reader count-pane 
-               :text (format nil "~A items" (length (compute-visible-rows (document interface))))))
+               :text (format nil "~A items" (length (compute-visible-rows (document interface)))))
+   (add-row-button push-button :reader add-row-button :text "+")
+   (add-column-button push-button :reader add-column-button :text "+"))
   
   ;; -- layouts ---------------------------------------------
   (:layouts
-   (main-layout column-layout '(contents-pane count-pane)
+   (header-layout row-layout '(nil add-column-button)
+                  :visible-min-height 48
+                  :visible-max-height 48
+                  :reader header-layout :border 4)
+   (footer-layout row-layout '(add-row-button count-pane)
+                  :visible-min-height 48
+                  :visible-max-height 48
+                  :reader footer-layout :border 4)
+   (main-layout column-layout '(header-layout contents-pane footer-layout)
                 :reader main-layout :border 4))
-
   
   ;; -- defaults ---------------------------------------------
   (:default-initargs :layout 'main-layout
+   :window-styles '(:textured-background)
    :width 800 :height 600))
 
 ;;; dummy method
@@ -73,3 +83,6 @@
                                                  :sort 'string-lessp
                                                  :reverse-sort 'string-greaterp))))
 
+;;; (defparameter $store (make-instance 'store :data-path "/Users/mikel/Desktop/Movies.delectus2"))
+;;; (defparameter $doc (make-instance 'document :store $store))
+;;; (defparameter $ui (contain (make-instance 'document-window :document $doc)))
