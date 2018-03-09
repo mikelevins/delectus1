ec30041189db6578fe44bd4ec97fb4d697c7f48f
mikel evins <mevins@me.com>
2018-03-08 19:04:22 -0600

    added a comment for file-pathname-p

--- src/data-engine/file-utils.lisp
+++ src/data-engine/file-utils.lisp
@@ -10,6 +10,11 @@
 
 (in-package :data)
 
+;;;  file-pathname-p (pathname)
+;;; ---------------------------------------------------------------------
+;;; returns true if the pathname's name or type part is nonempty
+;;; does not check whether the named file actually exists
+
 (defun file-pathname-p (pathname)
   (when pathname
     (let* ((pathname (pathname pathname))
