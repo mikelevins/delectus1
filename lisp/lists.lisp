6b4c402ae1b444c62a0b503dce869a758d201cb8
mikel evins <mikel@evins.net>
2018-05-01 12:43:15 +0000

    further details added to lists.lisp

--- lisp/lists.lisp
+++ lisp/lists.lisp
@@ -15,16 +15,18 @@
 (defun make-row (value-list)
   (mapcar #'identity value-list))
 
-(defun delectus-list (username columns rows)
+(defun delectus-list (title username columns rows &optional (note ""))
   (assert (lists-congruent-p columns rows)()
           "Some rows have lengths different from the number of columns in ~S"
           columns)
   `((:|user| . ,(username->dbname username))
     (:|type| . "List")
+    (:|title| . ,title)
+    (:|note| . ,note)
     (:|created_time| . ,(local-time:format-timestring nil (local-time:now)))
     (:|modified_time| . "")
     (:|columns| . ,(mapcar #'identity columns))
     (:|rows| . ,(mapcar #'make-row rows))))
 
-;;; (defparameter $minimovies (delectus-list "delectus" '("title" "star" "rating") '(("Bruce Almighty" "Jim Carrey" "***")("Home Alone" "Macauley Culkin" "***")("The Maltese Falcon" "Humphrey Bogart" "*****"))))
+;;; (defparameter $minimovies (delectus-list "Mini-Movies" "delectus" '("title" "star" "rating") '(("Bruce Almighty" "Jim Carrey" "***")("Home Alone" "Macauley Culkin" "***")("The Maltese Falcon" "Humphrey Bogart" "*****"))))
 
