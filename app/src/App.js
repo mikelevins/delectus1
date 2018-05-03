a35b4326e5bca534c22398e3e71b3eb0fb7b6d81
mikel evins <mevins@me.com>
2018-05-03 07:32:03 -0500

    Custmized listbox theme

--- app/src/App.js
+++ app/src/App.js
@@ -5,15 +5,25 @@ import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
+import {brown700, brown900, redA200} from 'material-ui/styles/colors';
 
 // delectus' own includes
 import "./App.css";
 import DelectusList from "./DelectusList";
 
+const muiTheme = getMuiTheme({
+  palette: {
+      primary1Color: brown700,
+      primary2Color: brown900,
+      accent1Color: redA200,
+      pickerHeaderColor: brown700,
+  },
+});
+
 class App extends Component {
   constructor() {
     super();
-    this.state = { theme: lightBaseTheme };
+    this.state = { theme: muiTheme };
   }
 
   render() {
