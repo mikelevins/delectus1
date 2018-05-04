f5b487f619acdb563382d7f4ae3ff963cda2c68d
mikel evins <mikel@evins.net>
2018-05-04 00:07:32 +0000

    added start of a simple API for talking to the delectus CouchDB instance

--- server/src/database.lisp
+++ server/src/database.lisp
@@ -5,3 +5,15 @@
 (defparameter *database-config-file*
   "/home/delectus/delectus-config.lisp")
 
+(defun initdb ()
+  (load *database-config-file*)
+  (clouchdb:set-connection :host *dbhost*
+                           :port *dbport*
+                           :protocol *dbprotocol*
+                           :user *dbuser*
+                           :password *dbpassword*))
+
+;;; (initdb)
+;;; (clouchdb:list-dbs)
+;;; (clouchdb:set-connection :name "_users")
+;;; (clouchdb:get-all-documents)
