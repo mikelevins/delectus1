7fa109febf9ccfc73b28b5e1d2a7e5c64ba938ea
mikel evins <mevins@me.com>
2018-05-04 15:33:55 -0500

    fixing up lecter to generate uuids for constructing couchdb documents

--- lecter/src/uuid.scm
+++ lecter/src/uuid.scm
@@ -10,6 +10,7 @@
 (define (make-uuid)
   (define hex
     '#(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\A #\B #\C #\D #\E #\F))
+  (random-source-randomize! default-random-source)
   (let ((n1 (random-integer 65536))
         (n2 (random-integer 65536))
         (n3 (random-integer 65536))
