51c42d20f0cef3a8cee8a87ab3bff2c6b7fbc794
mikel evins <mevins@me.com>
2018-05-05 17:05:35 -0500

    tinkering with Delectus 1 homepage

--- homepage1/src/App.js
+++ homepage1/src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 
+// Helmet for controlling the head section
+import { Helmet } from "react-helmet";
+
 // material-ui stuff
 import AppBar from "material-ui/AppBar";
 import Card from "material-ui/Card";
+import { GridList, GridTile } from "material-ui/GridList";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import lightBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 // delectus' own includes
 import "./App.css";
-
-const cardStyle = {
-  marginBottom: "1rem",
-  marginLeft: "15%",
-  marginRight: "15%",
-  marginTop: "1rem",
-  padding: "0.75rem",
-  textAlign: "center"
-};
+import "typeface-roboto";
 
 const appBarStyle = {
-  backgroundColor: "#4F3B27",
+  backgroundColor: "#4F3B27"
+};
+
+const rootStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-around"
+};
+
+const gridListStyle = {
+  width: "90%",
+  margin: "auto",
+  overflowY: "auto"
+};
+
+const cardStyle = {
+  margin: "auto",
+  paddingLeft: "1rem",
+  paddingRight: "1rem",
+  paddingTop: "1rem",
+  height: "100%",
+  textAlign: "center"
 };
 
 class App extends Component {
@@ -32,47 +49,55 @@ class App extends Component {
   render() {
     document.body.style.backgroundColor = "#dfd6c4";
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(this.state.theme)}>
-        <div>
-          <AppBar title="Delectus" style={appBarStyle} />
-          <Card style={cardStyle}>
-            <img src="icon.png" width="54" height="65" />
-            <p>Your lists. Your way.</p>
-            <p>
-              $1.99 on the{" "}
-              <a href="https://itunes.apple.com/us/app/delectus/id431505280?ls=1&mt=12">
-                Mac App Store.
-              </a>
-            </p>
-          </Card>
+      <div>
+        <Helmet>
+          <title>Delectus</title>
+        </Helmet>
+        <MuiThemeProvider muiTheme={getMuiTheme(this.state.theme)}>
+          <div style={rootStyle}>
+            <AppBar title="Delectus" style={appBarStyle} />
 
-          <Card style={cardStyle}>
-            <h2>Manual</h2>
-            <p>
-              Download the Delectus manual &nbsp;
-              <a href="https://delectus.evins.net/downloads/Delectus_manual.zip">
-                here
-              </a>.
-            </p>
-          </Card>
-
-          <Card style={cardStyle}>
-            <h2>Support</h2>
-            <p>
-              Click &nbsp;
-              <a href="mailto:delectus@evins.net?subject=About%20Delectus">
-                here
-              </a>{" "}
-              for help and support.
-            </p>
-            <p>or send email to delectus@evins.net.</p>
-          </Card>
-        </div>
-      </MuiThemeProvider>
+            <Card style={cardStyle}>
+              <img src="icon.png" alt="Delectus icon" width="54" height="65" />
+              <p>
+                <strong>Your lists. Your way.</strong>
+              </p>
+              <p>
+                $1.99 on the{" "}
+                <a href="https://itunes.apple.com/us/app/delectus/id431505280?ls=1&mt=12">
+                  Mac App Store.
+                </a>
+              </p>
+            </Card>
+            <Card style={cardStyle}>
+              <h2>Manual</h2>
+              <div>
+                <p>
+                  Download the Delectus manual &nbsp;
+                  <a href="https://delectus.evins.net/downloads/Delectus_manual.zip">
+                    here
+                  </a>.
+                </p>
+              </div>
+            </Card>
+            <Card style={cardStyle}>
+              <h2>Support</h2>
+              <div>
+                <p>
+                  Click &nbsp;
+                  <a href="mailto:delectus@evins.net?subject=About%20Delectus">
+                    here
+                  </a>{" "}
+                  for help and support.
+                </p>
+                <p>or send email to delectus@evins.net.</p>
+              </div>
+            </Card>
+          </div>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
 
 export default App;
-
-
