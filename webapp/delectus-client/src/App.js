bb32e6ad9be08324692a68a3f3971a5067c07438
mikel evins <mevins@me.com>
2018-10-30 10:25:17 -0500

    refactored into tidier components, cleaned up file structure

--- webapp/delectus-client/src/App.js
+++ webapp/delectus-client/src/App.js
@@ -1,99 +1,115 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// ---------------------------------------------------------
+// Styles for the components (included by the App)
+// ---------------------------------------------------------
+
 const css = `
 .Entry { 
   border: 1px solid black;
   border-collapse: collapse;
-  margin: 2em; 
+  margin: .5em; 
+  padding: 0.25em; 
   text-align: left;
 }
 
 .EntryField { 
   border: 1px solid black;
   border-collapse: collapse;
-  padding: 0.5em; 
 }
 `;
 
-function DocItem(props){
-  return <li>{props.name}</li>;
+// ---------------------------------------------------------
+// helper functions
+// ---------------------------------------------------------
+
+function syncWithRemoteDB(props) {
+  props.localDB.sync(props.remoteDB);
 }
 
+function refreshAllDocs(props) {
+  props.localDB.allDocs(
+    { include_docs: true, descending: true },
+    function (err, result) {
+      if (err) {
+        console.log("Error getting documents: ");
+        console.log(err);
+      } else {
+        var the_rows = result.rows;
+        props.app.setState({ allDocs: the_rows });
+      }
+    }
+  );
+}
+
+// ---------------------------------------------------------
+// DocEntry component
+// ---------------------------------------------------------
+
+function DocEntry(props) {
+  return (<li className="Entry">{props.name}</li>);
+}
+
+// ---------------------------------------------------------
+// DocList component
+// ---------------------------------------------------------
+
 function DocList(props) {
-  const docs = props.documents;
-  const docItems = docs.map(doc =>
-    <DocItem key={doc.id}
-      name={doc.id} />
+  const entries = props.documents;
+  const docEntries = entries.map((entry) =>
+    <DocEntry
+      key={entry.id}
+      name={entry.id} />
   );
 
   return (
-    <div className="App">
-      <style>{css}</style>
-      <h1>Opps Daily</h1>
+    <div className="DocList">
       <ul>
-        {docItems}
+        {docEntries}
       </ul>
     </div>
   );
+
 }
+
+// ---------------------------------------------------------
+// App component
+// ---------------------------------------------------------
 
 class App extends Component {
   state = {
-    users: [],
     allDocs: [],
     localDB: this.props.localPouchDB,
     remoteDB: this.props.remoteCouchDB
   }
 
   componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-    this.syncWithRemoteDB();
-  }
-
-  syncWithRemoteDB() {
-    this.state.localDB.sync(this.state.remoteDB).on('complete', function () {
-      // yay, we're in sync!
-      console.log("syncing with remote CouchDB")
-    }).on('error', function (err) {
-      // boo, we hit an error!
-      console.log("ERROR: sync failed")
+    syncWithRemoteDB({
+      localDB: this.state.localDB,
+      remoteDB: this.state.remoteDB
+    });
+    refreshAllDocs({
+      app: this,
+      localDB: this.state.localDB
     });
   }
 
-  getAllDocs() {
-    var app = this;
-    this.state.localDB.allDocs(
-      { include_docs: true, descending: true },
-      function (err, result) {
-        if (err) {
-          console.log("Error getting documents: ");
-          console.log(err);
-        } else {
-          var the_rows = result.rows;
-          app.setState({ allDocs: the_rows });
-        }
-      }
+  render() {
+    return (
+      <div className="App">
+        <style>{css}</style>
+        <h1>Opps Daily</h1>
+        <h3>document count: {this.state.allDocs.length}</h3>
+        <DocList documents={this.state.allDocs} />
+      </div>
     );
   }
 
-  formatFieldForDisplay(fName, fVal) {
-    var contentBanner = ' ** The Opportunity ------------------------------------------------------------  ';
-    if (fName == 'content') {
-      return (fVal.substring(contentBanner.length, 320) + '...');
-    } else {
-      return (fVal);
-    }
-  }
-
-  render() {
-    this.getAllDocs();
-    return (<DocList documents={this.state.allDocs} />);
-  }
-
-
 }
+
+// ---------------------------------------------------------
+// main export
+// ---------------------------------------------------------
 
 export default App;
