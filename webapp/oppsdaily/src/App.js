1d3c16ee426a78d5c5652006c26ea6d7c8508860
mikel evins <mevins@me.com>
2018-11-05 12:21:09 -0600

    moved oppsdaily's EditorField to its own source file

--- webapp/oppsdaily/src/App.js
+++ webapp/oppsdaily/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import EditorField from './EditorField.js';
 
 // ---------------------------------------------------------
 // helper functions
@@ -54,43 +55,6 @@ function hideEditor(props) {
   props.app.setState({ showEditor: false })
 }
 
-
-// ---------------------------------------------------------
-// EditorField component
-// ---------------------------------------------------------
-
-function EditorField(props) {
-  var labelText = props.docKey;
-  var valueText = props.doc[props.docKey];
-
-  if (props.docKey === "content") {
-    return (
-      <div className="Row">
-        <div className="Label">
-          <label>{labelText}:</label>
-        </div>
-        <div className="Cell">
-          <textarea
-            className="TextAreaCell"
-            name={props.docKey}
-            defaultValue={valueText} />
-        </div>
-      </div>);
-  } else {
-    return (
-      <div className="Row">
-        <div className="Label">
-          <label>{labelText}:</label>
-        </div>
-        <div className="Cell">
-          <input className="InputCell"
-            name={props.docKey}
-            type="text" defaultValue={valueText} />
-        </div>
-      </div>
-    );
-  }
-}
 
 // ---------------------------------------------------------
 // DocCompactField component
---
+++ webapp/oppsdaily/src/EditorField.js
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+import './App.css';
+
+// ---------------------------------------------------------
+// EditorField component
+// ---------------------------------------------------------
+
+function EditorField(props) {
+    var labelText = props.docKey;
+    var valueText = props.doc[props.docKey];
+  
+    if (props.docKey === "content") {
+      return (
+        <div className="Row">
+          <div className="Label">
+            <label>{labelText}:</label>
+          </div>
+          <div className="Cell">
+            <textarea
+              className="TextAreaCell"
+              name={props.docKey}
+              defaultValue={valueText} />
+          </div>
+        </div>);
+    } else {
+      return (
+        <div className="Row">
+          <div className="Label">
+            <label>{labelText}:</label>
+          </div>
+          <div className="Cell">
+            <input className="InputCell"
+              name={props.docKey}
+              type="text" defaultValue={valueText} />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default EditorField;
