7102485da6f20a824897f1b00a54e364f52d2f22
mikel evins <mevins@me.com>
2018-11-06 12:35:20 -0600

    oppsdaily: fixed DocEditor to make certain fields non-editable

--- webapp/oppsdaily/src/EditorField.js
+++ webapp/oppsdaily/src/EditorField.js
@@ -8,11 +8,24 @@ import './App.css';
 // Displays CouchDB document fields for editing
 // BUG: special-cases the 'content' field
 
+const readOnlyFieldNames = ['_rev', '_id', 'date_received', 'message_id'];
+
 function EditorField(props) {
     var labelText = props.docKey;
     var valueText = props.doc[props.docKey];
 
-    if (props.docKey === "content") {
+    if (readOnlyFieldNames.includes(props.docKey)) {
+        return (
+            <div className="Row">
+                <div className="Label">
+                    <label>{labelText}:</label>
+                </div>
+                <div className="Cell">
+                    <span>{valueText}</span>
+                </div>
+            </div>
+        );
+    } else if (props.docKey === "content") {
         return (
             <div className="Row">
                 <div className="Label">
