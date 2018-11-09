dfc0d686454bbe6f0bebd497de543d4463672269
mikel evins <mevins@me.com>
2018-11-09 12:16:15 -0600

    oppsdaily: refactoring and fixing up the Opps Daily AppBar

---
+++ webapp/oppsdaily/src/OppsDailyAppBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function OppsDailyAppBar(props) {
+  const { classes } = props;
+  const statusText = props.statusText;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" color="inherit" className={classes.grow}>
+          Opps Daily
+          </Typography>
+          <Typography variant="subheading" color="inherit">
+          {statusText}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+OppsDailyAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(OppsDailyAppBar);
