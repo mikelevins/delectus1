d88f7048134bc81d1a1fbdcaab3655cc0507afb1
mikel evins <mevins@me.com>
2018-11-30 10:22:59 -0600

    couchinspector: fleshed out the auth dialog panel

--- webapp/couch-inspector/src/BrowserAuthPane.js
+++ webapp/couch-inspector/src/BrowserAuthPane.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 // BrowserAuthPane styles
 // ---------------------------------------------------------
@@ -9,19 +10,68 @@ import Button from '@material-ui/core/Button';
 const styles = {
     browserPane: {
         border: '1px solid black',
-        height: '24rem',
+        height: '14rem',
         overflow: 'auto',
         padding: '6px',
-        width: '100%',
+        width: '24rem',
     },
     browserPaneTitle: {
         fontWeight: 'bold',
         height: '1rem',
     },
     button: {
+        marginLeft: '1rem',
+        marginTop: '1.5rem',
         textTransform: 'none'
-    }
+    },
+    textField: { 
+        marginLeft: '1rem',
+        marginTop: '1rem',
+        width: '95%',
+     },
 };
+
+// Auxiliary components
+// ---------------------------------------------------------
+
+function PasswordField(props) {
+    return (
+        <div>
+            <TextField
+                id="password"
+                label='Password:'
+                style={styles.textField}
+                type='password'
+                defaultValue=''
+            />
+        </div>
+    );
+}
+
+function UsernameField(props) {
+    return (
+        <div>
+            <TextField
+                id="username"
+                label='Username:'
+                style={styles.textField}
+                defaultValue=''
+            />
+        </div>
+    );
+}
+
+function LoginButton(props) {
+    const pane = props.pane;
+    return (
+        <Button
+            style={styles.button}
+            variant="contained"
+            color="primary">
+            Log In
+        </Button>
+    );
+}
 
 // BrowserAuthPane class
 // ---------------------------------------------------------
@@ -37,8 +87,11 @@ class BrowserAuthPane extends Component {
 
         return (
             <div>
-                <div>
-                    <p style={styles.browserPaneTitle}>Log In</p>
+                <div><p style={styles.browserPaneTitle}>Log In</p></div>
+                <div style={styles.browserPane}>
+                <UsernameField />
+                <PasswordField />
+                <LoginButton />
                 </div>
             </div>
         );
