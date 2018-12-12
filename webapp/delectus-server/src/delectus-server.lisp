cd6b5dd2ac0388240c52ff969c37d031e417d5bb
mikel evins <mevins@me.com>
2018-12-12 13:23:12 -0600

    delectus-server: got ultra-simple hunchentoot server up and running

--- webapp/delectus-server/src/delectus-server.lisp
+++ webapp/delectus-server/src/delectus-server.lisp
@@ -4,18 +4,22 @@
 
 (defparameter *delectus-server* nil)
 
-(defun start-server (&key (port 8080))
+(defun start-server (&key (port (server-port)))
   (let* ((acceptor (make-instance 'hunchentoot:easy-acceptor
                                   :port port))
          (dispatcher (snooze:make-hunchentoot-app)))
     ;; 1. setf (acceptor-document-root acceptor) to the public directory
-    ;; 2. pushnew the dispatcher onto hunchentoot:*dispatch-table*
-    ;; 3. stuff acceptor into *delectus-server*
-    ;; 4. hunchentoot:start acceptor
+    (setf (hunchentoot:acceptor-document-root acceptor)
+          (server-pathname :public))
+    ;; 2. push the dispatcher onto hunchentoot:*dispatch-table*
+    (push dispatcher hunchentoot:*dispatch-table*)
+    ;; 3. hunchentoot:start acceptor
+    (hunchentoot:start acceptor)
+    ;; 4. return the acceptor
     acceptor))
 
-(defun stop-server ()
-  (hunchentoot:stop *delectus-server*))
+(defun stop-server (server)
+  (hunchentoot:stop server))
 
 ;;; (defparameter *delectus-server* (start-server :port (server-port)))
 ;;; (stop-server *delectus-server*)
