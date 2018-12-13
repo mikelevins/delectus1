f4b2993339af40310338027158543b6e98b86a0b
mikel evins <mevins@me.com>
2018-12-13 12:17:25 -0600

    delectus-server: added environment constructor for template processing

--- webapp/delectus-server/src/routes.lisp
+++ webapp/delectus-server/src/routes.lisp
@@ -2,7 +2,8 @@
 
 (in-package #:delectus-server)
 
-(defun default-environment () nil)
+(defun default-environment ()
+  (environment))
 
 (defroute homepage (:get "text/html")
           (process-template (path :templates "index.tmpl")
