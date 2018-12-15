8713bf06c738122145804f73a0d6e7328ac92726
mikel evins <mevins@me.com>
2018-12-15 07:45:18 -0600

    delectus-client: using material ui components to layout the landing page

--- webapp/delectus-client/src/Landing.js
+++ webapp/delectus-client/src/Landing.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import Paper from '@material-ui/core/Paper';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
 
 class Landing extends Component {
-
     render() {
+        const { classes } = this.props;
         return (
-            <div id='appBackdrop' className='appBackdrop'>
-                <Paper>
-                    <div className='top-bar'>
-                        <Typography component='h1'>Delectus</Typography>
-                        <div className='top-bar-right'>
-                            <a href='/login'>Log in</a>
-                            <a href='/signup'>Sign up</a>
-                        </div>
+            <div id='appBackdrop' className={classes.root}>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant='title' color='inherit' className={classes.grow}>
+                            Delectus
+                        </Typography>
+                        <Button color="inherit">Log in</Button>
+                        <Button color="inherit">Sign up</Button>
+                    </Toolbar>
+                    <Paper>
                         <Typography component='p'>Welcome to Delectus</Typography>
-                    </div>
-                </Paper>
+                    </Paper>
+                </AppBar>
             </div>
         );
     }
@@ -27,4 +51,4 @@ class Landing extends Component {
 // exports
 // ---------------------------------------------------------
 
-export default Landing;
+export default withStyles(styles)(Landing);
