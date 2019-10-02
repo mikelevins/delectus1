8b4b78107391fbdc81d3ba11573c388e038d1a27
mikel evins <mevins@me.com>
2019-10-02 15:22:02 -0500

    added initdelectus1engine() appdelegate startup code

--- Delectus 2/Delectus 2/AppDelegate.swift
+++ Delectus 2/Delectus 2/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        init_delectus1_engine()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
