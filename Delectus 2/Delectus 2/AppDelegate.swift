ab4169b8a3eadbd6bc86b96aae42553cd10bacf5
mikel evins <mevins@me.com>
2019-10-03 10:15:27 -0500

    renamed DxStore to StorageFunctions

--- Delectus 2/Delectus 2/AppDelegate.swift
+++ Delectus 2/Delectus 2/AppDelegate.swift
@@ -11,8 +11,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         init_delectus1_engine()
         let collections = knownCollections()
