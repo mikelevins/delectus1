c03568348c107aef171c97021c2d8b3a9aeb382c
mikel evins <mevins@me.com>
2019-10-10 16:19:31 -0500

    added the clojure implementation of the Delectus 2 API server

---
+++ delectus-api-server/project.clj
@@ -0,0 +1,21 @@
+(defproject delectus-api-server "0.1.0-SNAPSHOT"
+  :description "Delectus 2 API server"
+  :url "http://delect.us"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 ;; Compojure - A basic routing library
+                 [compojure "1.6.1"]
+                 ;; Our Http library for client/server
+                 [http-kit "2.3.0"]
+                 ;; Ring defaults - for query params etc
+                 [ring/ring-defaults "0.3.2"]
+                 ;; Clojure data.JSON library
+                 [org.clojure/data.json "0.2.6"]
+                 ;; Couchbase Java client
+                 [com.couchbase.client/java-client "2.7.9"]]
+
+  :main ^:skip-aot delectus-api-server.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ delectus-api-server/src/delectus_api_server/core.clj
@@ -0,0 +1,58 @@
+(ns delectus-api-server.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json]
+            )
+  (:gen-class))
+
+;;; ---------------------------------------------------------------------
+;;; 
+;;; ---------------------------------------------------------------------
+
+(defn landing-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Delectus 2 API Server, v 0.1"})
+
+(defn hello-name [req]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body (->
+          (clojure.pprint/pprint req)
+          (str "Hello, " (:name (:params req))))})
+
+(defn couch-page [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (let [couch (com.couchbase.client.java.CouchbaseCluster/create ["mars.local"])
+               diagnostics (.diagnostics couch)]
+           (.authenticate couch "admin" "password")
+           (.exportToJson diagnostics))})
+
+;;; ---------------------------------------------------------------------
+;;; routes
+;;; ---------------------------------------------------------------------
+
+(defroutes app-routes
+  (GET "/" [] landing-page)
+  (GET "/hello" [] hello-name)
+  (GET "/couch" [] couch-page)
+  (route/not-found "Error, page not found!"))
+
+;;; ---------------------------------------------------------------------
+;;; main server program
+;;; ---------------------------------------------------------------------
+
+(defn -main
+  "This is our main entry point"
+  [& args]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ;; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ;; Run the server without ring defaults
+    ;;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
