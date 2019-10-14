8445cb4b433befd59e2682b72431ec2c2eae2962
mikel evins <mevins@me.com>
2019-10-14 10:56:31 -0500

    made the API server get its cluster name from the config; added test code to test CouchbaseMap objects

--- delectus-api-server/src/delectus_api_server/core.clj
+++ delectus-api-server/src/delectus_api_server/core.clj
@@ -39,7 +39,7 @@
 ;;; couchbase connection
 ;;; ---------------------------------------------------------------------
 
-(defonce +couchbase-cluster-name+ "mars.local")
+(defonce +couchbase-cluster-name+ (:delectus-db-server (delectus-configuration)))
 (defonce +couchbase-cluster+ (atom nil))
 
 (defn couchbase-cluster []
@@ -164,11 +164,21 @@
 ;;; (def $conf (delectus-configuration))
 ;;; (.authenticate $couch (:delectus-admin-user $conf)(:delectus-admin-password $conf))
 ;;; (def $bucket (.openBucket $couch "collection-test"))
+
+;;; List
 ;;; (def $listid "test_list_1")
 ;;; (def $flavor-list (new com.couchbase.client.java.datastructures.collections.CouchbaseArrayList $listid $bucket))
 ;;; (.size $flavor-list)
 ;;; (.add $flavor-list "Apple")
 ;;; (.add $flavor-list "Banana")
+
+;;; Map
+;;; (def $mapid "test_map_1")
+;;; (def $mapval { "Apple" "red", "Banana" "yellow", "Cherry" "red"})
+;;; (def $flavor-map (new com.couchbase.client.java.datastructures.collections.CouchbaseMap $mapid $bucket $mapval))
+;;; (def $flavor-map (new com.couchbase.client.java.datastructures.collections.CouchbaseMap $mapid $bucket))
+;;; (.size $flavor-map)
+
 
 ;;; ---------------------------------------------------------------------
 ;;; routes
