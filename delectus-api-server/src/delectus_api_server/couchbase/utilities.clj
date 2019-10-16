1088ea341d9e472ed4b8a2710d6955e22bd03a97
mikel evins <mevins@me.com>
2019-10-16 07:31:41 -0500

    add ->JsonObject to couch utils

--- delectus-api-server/src/delectus_api_server/couchbase/utilities.clj
+++ delectus-api-server/src/delectus_api_server/couchbase/utilities.clj
@@ -7,3 +7,6 @@
 (defn ensure-primary-index [bucket]
   ;; create a N1QL primary index, unless it already exists
   (.createN1qlPrimaryIndex (.bucketManager bucket) true false))
+
+(defn ->JsonObject [mapdata]
+  (com.couchbase.client.java.document.json.JsonObject/from mapdata))
