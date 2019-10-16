6003591ffc5965db74a76aee5bbc744a00fcdc64
mikel evins <mevins@me.com>
2019-10-16 07:10:24 -0500

    refactoring: travel-sample handlers to their own file

--- delectus-api-server/src/delectus_api_server/core.clj
+++ delectus-api-server/src/delectus_api_server/core.clj
@@ -11,74 +11,10 @@
             [delectus-api-server.utilities :as utils]
             [delectus-api-server.route-handlers :as handlers]
             [delectus-api-server.couchbase.utilities :as couch-utils]
-            [delectus-api-server.couchbase.route-handlers :as couch-handlers])
+            [delectus-api-server.couchbase.route-handlers :as couch-handlers]
+            [delectus-api-server.couchbase.travel-sample.route-handlers :as travel-handlers])
   (:gen-class))
 
-;;; ---------------------------------------------------------------------
-;;; travel-sample handlers and support functions
-;;; ---------------------------------------------------------------------
-
-;;; returns: ("airline" "airport" "hotel" "landmark" "route")
-(defn document-types [req bucket-name]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (let [couch (config/couchbase-cluster)
-               configuration (config/delectus-configuration)]
-           (.authenticate couch
-                          (:travel-sample-user configuration)
-                          (:travel-sample-password configuration))
-           (let [bucket (.openBucket couch bucket-name)]
-             (couch-utils/ensure-primary-index bucket)
-             (let [select-expression (pp/cl-format nil
-                                                   "SELECT type FROM `~A` WHERE type IS NOT MISSING"
-                                                   bucket-name)
-                   result (.query bucket (com.couchbase.client.java.query.N1qlQuery/simple select-expression))
-                   vals (distinct (map (fn [r] (.value r)) result))
-                   objs (map (fn [v](:type (json/read-json (.toString v)))) vals)]
-               (json/write-str objs))))})
-
-(defn objects-of-type [req bucket-name type-name]
-  (let [couch (config/couchbase-cluster)
-        configuration (config/delectus-configuration)]
-    (.authenticate couch
-                   (:travel-sample-user configuration)
-                   (:travel-sample-password configuration))
-    (let [bucket (.openBucket couch bucket-name)]
-      (couch-utils/ensure-primary-index bucket)
-      (let [limit (or (:limit (:params req)) 10)
-            offset (or (:offset (:params req)) 0)
-            select-expr (pp/cl-format nil
-                                      "SELECT * FROM `~A` WHERE type = \"~A\" LIMIT ~A OFFSET ~A"
-                                      bucket-name type-name limit offset)
-            result (.query bucket (com.couchbase.client.java.query.N1qlQuery/simple select-expr))
-            vals (map (fn [r](get (json/read-json (.toString (.value r))) (keyword bucket-name)))
-                      result)]
-        (json/write-str vals)))))
-
-(defn airlines [req]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "airline")})
-
-(defn airports [req]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "airport")})
-
-(defn hotels [req]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "hotel")})
-
-(defn landmarks [req]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "landmark")})
-
-(defn travel-routes [req]
-  {:status 200
-   :headers {"Content-type" "application/json"}
-   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "route")})
 
 ;;; ---------------------------------------------------------------------
 ;;; collection-test handlers and support functions
@@ -144,12 +80,15 @@
   (GET "/status" [] couch-handlers/status)
   ;; travel-sample test routes
   ;; -------------------
-  (GET "/document-types" [] (fn [req] (document-types req (:travel-sample-bucket-name (config/delectus-configuration)))))
-  (GET "/airlines" [] airlines)
-  (GET "/airports" [] airports)
-  (GET "/hotels" [] hotels)
-  (GET "/landmarks" [] landmarks)
-  (GET "/routes" [] travel-routes)
+  (GET "/document-types" [] (fn [req]
+                              (travel-handlers/document-types
+                               req
+                               (:travel-sample-bucket-name (config/delectus-configuration)))))
+  (GET "/airlines" [] travel-handlers/airlines)
+  (GET "/airports" [] travel-handlers/airports)
+  (GET "/hotels" [] travel-handlers/hotels)
+  (GET "/landmarks" [] travel-handlers/landmarks)
+  (GET "/routes" [] travel-handlers/travel-routes)
   ;; Delectus API routes
   ;; -------------------
   
---
+++ delectus-api-server/src/delectus_api_server/couchbase/travel_sample/route_handlers.clj
@@ -0,0 +1,72 @@
+(ns delectus-api-server.couchbase.travel-sample.route-handlers
+  (:require [clojure.pprint :as pp]
+            [clojure.data.json :as json]
+            [delectus-api-server.configuration :as config]
+            [delectus-api-server.couchbase.utilities :as couch-utils]
+            ))
+
+;;; ---------------------------------------------------------------------
+;;; travel-sample handlers and support functions
+;;; ---------------------------------------------------------------------
+
+;;; returns: ("airline" "airport" "hotel" "landmark" "route")
+(defn document-types [req bucket-name]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (let [couch (config/couchbase-cluster)
+               configuration (config/delectus-configuration)]
+           (.authenticate couch
+                          (:travel-sample-user configuration)
+                          (:travel-sample-password configuration))
+           (let [bucket (.openBucket couch bucket-name)]
+             (couch-utils/ensure-primary-index bucket)
+             (let [select-expression (pp/cl-format nil
+                                                   "SELECT type FROM `~A` WHERE type IS NOT MISSING"
+                                                   bucket-name)
+                   result (.query bucket (com.couchbase.client.java.query.N1qlQuery/simple select-expression))
+                   vals (distinct (map (fn [r] (.value r)) result))
+                   objs (map (fn [v](:type (json/read-json (.toString v)))) vals)]
+               (json/write-str objs))))})
+
+(defn objects-of-type [req bucket-name type-name]
+  (let [couch (config/couchbase-cluster)
+        configuration (config/delectus-configuration)]
+    (.authenticate couch
+                   (:travel-sample-user configuration)
+                   (:travel-sample-password configuration))
+    (let [bucket (.openBucket couch bucket-name)]
+      (couch-utils/ensure-primary-index bucket)
+      (let [limit (or (:limit (:params req)) 10)
+            offset (or (:offset (:params req)) 0)
+            select-expr (pp/cl-format nil
+                                      "SELECT * FROM `~A` WHERE type = \"~A\" LIMIT ~A OFFSET ~A"
+                                      bucket-name type-name limit offset)
+            result (.query bucket (com.couchbase.client.java.query.N1qlQuery/simple select-expr))
+            vals (map (fn [r](get (json/read-json (.toString (.value r))) (keyword bucket-name)))
+                      result)]
+        (json/write-str vals)))))
+
+(defn airlines [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "airline")})
+
+(defn airports [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "airport")})
+
+(defn hotels [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "hotel")})
+
+(defn landmarks [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "landmark")})
+
+(defn travel-routes [req]
+  {:status 200
+   :headers {"Content-type" "application/json"}
+   :body (objects-of-type req (:travel-sample-bucket-name (config/delectus-configuration)) "route")})
