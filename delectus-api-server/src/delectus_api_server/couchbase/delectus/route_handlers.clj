a3002b956a424d94d06d55345a99282dd616207b
mikel evins <mevins@me.com>
2019-10-22 15:32:36 -0500

    added API endpoint for /users

--- delectus-api-server/src/delectus_api_server/couchbase/delectus/route_handlers.clj
+++ delectus-api-server/src/delectus_api_server/couchbase/delectus/route_handlers.clj
@@ -1,6 +1,7 @@
 (ns delectus-api-server.couchbase.delectus.route-handlers
   (:require [clojure.pprint :as pp]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [delectus-api-server.couchbase.delectus.users :refer [list-delectus-users]]))
 
 ;;; ---------------------------------------------------------------------
 ;;; delectus handlers
@@ -12,14 +13,10 @@
    :body    (html [:h1 "Delectus 2"]
                   [:p "version 0.1"])})
 
-;; (defn users [req]
-;;   {:status  200
-;;    :headers {"Content-Type" "application/json"}
-;;    :body    (let [found (delectus-users)]
-;;               (json/write-str found))})
-
 (defn users [req]
   {:status  200
    :headers {"Content-Type" "application/json"}
-   :body    (json/write-str "/users not yet implemented")})
+   :body    (let [found (list-delectus-users)
+                  emails (map #(get % "primary-email") found)]
+              (json/write-str emails))})
 
