8bcc690189f7f940596554582dfdf073eb3bd1d7
mikel evins <mevins@me.com>
2019-10-31 17:19:55 -0500

    learning how to use auth middleware with http-kit

--- delectus-api-server/src/delectus_api_server/couchbase/delectus/route_handlers.clj
+++ delectus-api-server/src/delectus_api_server/couchbase/delectus/route_handlers.clj
@@ -10,15 +10,8 @@
             [delectus-api-server.couchbase.delectus.api :as api]))
 
 ;;; ---------------------------------------------------------------------
-;;; delectus handlers
+;;; support functions
 ;;; ---------------------------------------------------------------------
-
-(defn root [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    (html [:h1 "Delectus 2"]
-                  [:p "version 0.1"])})
-
 
 (defn login-user [email password]
   (let [found-user (users/user-from-email email)]
@@ -28,11 +21,22 @@
         false)
       false)))
 
+;;; ---------------------------------------------------------------------
+;;; delectus handlers
+;;; ---------------------------------------------------------------------
+
+(defn root [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (html [:h1 "Delectus 2"]
+                  [:p "version 0.1"])})
+
 (defn login [req]
   (let [params (:params req)
         supplied-email (:email params)
         supplied-password (:password params)
         found-user (login-user supplied-email supplied-password)]
+    (prn "supplied-password: " supplied-password)
     (if found-user
       {:status  200
        :headers {"Content-Type" "application/json"}
