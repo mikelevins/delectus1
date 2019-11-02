01a9d936446e1c4f5c22f98a9050bd149bd1c45c
mikel evins <mevins@me.com>
2019-11-02 12:12:11 -0500

    working on webtest support for auth when making API requests

--- webtest/src/App.svelte
+++ webtest/src/App.svelte
@@ -1,11 +1,11 @@
 <script>
- let users = [];
- function getUsers() {
-     fetch('http://mars.local:9000/delectus/users').then((resp) => resp.json()).then((data) => users=data);
- }
 </script>
 
 <h1>Delectus Test Client</h1>
 <p><em>version 2.0d1</em></p>
 
-<p><button on:click={getUsers}>delectus/users</button> [{users}]</p>
+<form method="get" action="http://mars.local:9000/delectus/login">
+    <p><input type="text" placeholder="email" name="email" /></p>
+    <p><input type="password" placeholder="password" name="password" /></p>
+    <p><input type="submit" value="login" /></p>
+</form>
