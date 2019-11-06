c7fcb91f26e5283963d36507d36233e1f2510162
mikel evins <mevins@me.com>
2019-11-06 07:47:35 -0600

    adding some explanatory notes to collection-add-list

--- delectus-api-server/src/delectus_api_server/api.clj
+++ delectus-api-server/src/delectus_api_server/api.clj
@@ -118,6 +118,7 @@
         collection-doc (get-document bucket collection-id)
         list-doc (get-document bucket list-id)]
 
+    ;; make sure the list and collection actually exist
     (if (nil? collection-doc) (throw (ex-info "No such collection" (ex-info {:id collection-id}))))
     (if (nil? list-doc) (throw (ex-info "No such list" (ex-info {:id list-id}))))
 
@@ -125,18 +126,21 @@
           collection-ownerid (.get found-collection "owner-id")
           found-list (.content list-doc)
           list-ownerid (.get found-list "owner-id")]
-      
+
+      ;; make sure the user owns the list and collection
       (if-not (= userid collection-ownerid)
         (throw (ex-info "Cannot update collection" (ex-info {:reason "wrong collection owner"}))))
       (if-not (= userid list-ownerid)
         (throw (ex-info "Cannot update list" (ex-info {:reason "wrong list owner"}))))
 
+      ;; prepare to add the list to the collection
       (let [old-collection-map (into {} (.toMap found-collection))
             old-collection-items (into {} (get old-collection-map "items"))
             old-collection-indexes (map edn/read-string (keys old-collection-items))
             new-index (if (empty? old-collection-indexes) 0 (+ 1 (apply max old-collection-indexes)))
             new-list-id (.get found-list "id")]
 
+        ;; don't add the list if it's already in the collection
         (if (some #{new-list-id} (vals old-collection-items))
           collection-id
           (do (let [new-collection-items (merge old-collection-items {(str new-index) new-list-id})
