7bd4d523439149cae698cb0e30703e775bfb63b6
mikel evins <mevins@me.com>
2019-11-06 22:34:02 -0600

    fixed a bug in error-if
    was calling ex-info on ex-info >.<

--- delectus-api-server/src/delectus_api_server/errors.clj
+++ delectus-api-server/src/delectus_api_server/errors.clj
@@ -1,21 +1,21 @@
 (ns delectus-api-server.errors)
 
 (defn error [message context]
-  (throw (ex-info message (ex-info (merge context {:error-signaled-by 'error})))))
+  (throw (ex-info message (merge context {:error-signaled-by 'error}))))
 
 (defn error-if-empty [value message context]
   (if (nil? value)
-    (throw (ex-info message (ex-info (merge context {:error-signaled-by 'error-if-empty}))))))
+    (throw (ex-info message (merge context {:error-signaled-by 'error-if-empty})))))
 
 (defn error-if-nil [value message context]
   (if (nil? value)
-    (throw (ex-info message (ex-info (merge context {:error-signaled-by 'error-if-nil}))))))
+    (throw (ex-info message (merge context {:error-signaled-by 'error-if-nil})))))
 
 (defn error-if [test-val message context]
   (if test-val
-    (throw (ex-info message (ex-info (merge context {:error-signaled-by 'error-if}))))))
+    (throw (ex-info message (merge context {:error-signaled-by 'error-if})))))
 
 (defn error-if-not [test-val message context]
   (if-not test-val
-    (throw (ex-info message (ex-info (merge context {:error-signaled-by 'error-if-not}))))))
+    (throw (ex-info message (merge context {:error-signaled-by 'error-if-not})))))
 
