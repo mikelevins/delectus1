a1046c8a8d0d479dce366f3fe746774d2c96cdf0
mikel evins <mevins@me.com>
2019-11-07 07:56:24 -0600

    added undeletes in route (commented out for now)

--- delectus-api-server/src/delectus_api_server/routes.clj
+++ delectus-api-server/src/delectus_api_server/routes.clj
@@ -42,6 +42,7 @@
   (GET "/delectus/collection_add_list" [] handlers/collection-add-list)       ; api/collection-add-list
   (GET "/delectus/collection_remove_list" [] handlers/collection-remove-list) ; api/collection-remove-list
   ;; (GET "/delectus/delete_collection" [] handlers/delete-collection         ; api/mark-collection-deleted
+  ;; (GET "/delectus/undelete_collection" [] handlers/undelete-collection     ; api/mark-collection-deleted
   ;; (GET "/delectus/collection_name" [] handlers/collection-name             ; api/get-collection-name
   ;; (GET "/delectus/rename_collection" [] handlers/rename-collection         ; api/update-collection-name
   ;; (GET "/delectus/collection_lists" [] handlers/collection-lists           ; api/get-collection-lists
@@ -54,6 +55,7 @@
   (GET "/delectus/list_with_id" [] handlers/list-with-id)                      ; api/find-list-by-id
   ;; (GET "/delectus/new_list" [] handlers/new-list)                           ; api/create-list
   ;; (GET "/delectus/delete_list" [] handlers/delete-list)                     ; api/mark-list-deleted
+  ;; (GET "/delectus/undelete_list" [] handlers/undelete-list)                 ; api/mark-list-deleted
   ;; (GET "/delectus/list_name" [] handlers/list-name                          ; api/list-name
   ;; (GET "/delectus/rename_list" [] handlers/rename-list                      ; api/update-list-name
   ;; (GET "/delectus/list_columns" [] handlers/list-columns                    ; api/list-columns
@@ -61,12 +63,14 @@
   ;; (GET "/delectus/column_with_id" [] handlers/column-with-id                ; api/find-column-by-id
   ;; (GET "/delectus/new_column" [] handlers/new-column                        ; api/list-add-column
   ;; (GET "/delectus/delete_column" [] handlers/delete-column                  ; api/mark-column-deleted
+  ;; (GET "/delectus/undelete_column" [] handlers/undelete-column              ; api/mark-column-deleted
   ;; (GET "/delectus/column_name" [] handlers/column-name                      ; api/column-name
   ;; (GET "/delectus/rename_column" [] handlers/rename-column                  ; api/update-column-name
   ;; (GET "/delectus/list_items" [] handlers/list_items                        ; api/list-items
   ;; (GET "/delectus/item_with_id" [] handlers/item-with-id                    ; api/find-item-by-id
   ;; (GET "/delectus/new_item" [] handlers/new-item                            ; api/list-add-item
   ;; (GET "/delectus/delete_item" [] handlers/delete-item                      ; api/mark-item-deleted
+  ;; (GET "/delectus/undelete_item" [] handlers/undelete-item                  ; api/mark-item-deleted
   ;; (GET "/delectus/item_column_value" [] handlers/item-column-value          ; api/item-column-value
   ;; (GET "/delectus/set_item_column_value" [] handlers/set-item-column-value  ; api/update-item-column-value
   
