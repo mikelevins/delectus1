6eb51ceff6adbe33dde530d523a1e63e9a00127b
mikel evins <mevins@me.com>
2019-12-02 11:12:59 -0600

    added svelte admin app

---
+++ admin-webapp/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+ import { authorization } from "./auth.js";
+ import Login from "./Login.svelte";
+</script>
+
+<h1>Delectus 2.0 Admin</h1>
+<p><em>version 2.0d1</em></p>
+
+{#if $authorization}
+    <p>App goes here</p>
+{:else}
+    <Login/>
+{/if}
+
+
