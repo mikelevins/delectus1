e3542ec30f5ab0812d6c8bddc3edcf1400ed6ffe
mikel evins <mevins@me.com>
2019-12-11 10:32:33 -0600

    adding code to handle auth tokens in api requests

--- delectus-api/src/delectus_api/handler.clj
+++ delectus-api/src/delectus_api/handler.clj
@@ -16,10 +16,17 @@
    [schema.core :as s]
    [tick.alpha.api :as t]))
 
+;;; ---------------------------------------------------------------------
+;;; schemata
+;;; ---------------------------------------------------------------------
 
 (s/defschema LoginRequest
   {:email s/Str
    :password s/Str})
+
+;;; ---------------------------------------------------------------------
+;;; finding registered users
+;;; ---------------------------------------------------------------------
 
 (defn email->user [email]
   (let [found (couchio/find-objects
@@ -33,6 +40,10 @@
 ;;; (email->user "mikel@evins.net")
 ;;; (email->user "greer@evins.net")
 ;;; (email->user "nobody@nowhere.net")
+
+;;; ---------------------------------------------------------------------
+;;; auth
+;;; ---------------------------------------------------------------------
 
 ;;; tokens
 ;;; ---------------------------------------------------------------------
@@ -71,17 +82,28 @@
 
 ;;; (def $token (make-auth-token (email->user "mikel@evins.net") "127.0.0.1"))
 ;;; (def $token-map (decode-auth-token $token))
+;;; (decode-auth-token "foo")
 
 (defn authenticate-user [email password]
   (let [found-user (email->user email)]
     (if found-user
-      (if (hashers/check password (.get found-user "password-hash"))
+      (if (hashers/check password (.get found-user +password-hash-attribute+))
         found-user
         nil)
       nil)))
 
 ;;; (authenticate-user "mikel@evins.net" "foo")
 ;;; (authenticate-user "nobody@evins.net" "foo")
+
+(defn authorized? [req]
+  (let [headers (:headers req)
+        authorization (get headers "authorization")]
+    (pp/cl-format true "~%Authorization: ~S~%" authorization)
+    true))
+
+;;; ---------------------------------------------------------------------
+;;; the api
+;;; ---------------------------------------------------------------------
 
 (def app
   (api
@@ -105,8 +127,19 @@
        :return {:token s/Str}
        :summary "authenticates a Delectus user"
        (let [remote-addr (:remote-addr req)
-                   maybe-auth (authenticate-user email password)]
-               (if maybe-auth
-                 (ok {:token (make-auth-token maybe-auth remote-addr)})
-                 (unauthorized "Login failed")))))))
+             maybe-auth (authenticate-user email password)]
+         (if maybe-auth
+           (ok {:token (make-auth-token maybe-auth remote-addr)})
+           (unauthorized "Login failed"))))
+
+     (GET "/userid/:email" req
+       :path-params [email :- s/Str]
+       :return s/Str
+       :summary "Returns the userid for the offered email address"
+       (if (authorized? req)
+         (let [found-user (email->user email)]
+           (if found-user
+             (ok (.get found-user +id-attribute+))
+             (not-found (str "No such user: " email))))
+         (unauthorized))))))
 
