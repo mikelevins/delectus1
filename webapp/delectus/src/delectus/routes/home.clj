0c822c4a4e00031453754260daa5d7253cfe78d6
mikel evins <mevins@me.com>
2020-01-04 07:54:20 -0600

    experimenting with different templates

--- webapp/delectus/src/delectus/routes/home.clj
+++
@@ -1,9 +0,0 @@
-(ns delectus.routes.home
-  (:require [compojure.core :refer :all]
-            [delectus.views.layout :as layout]))
-
-(defn home []
-  (layout/common [:h1 "Delectus"]))
-
-(defroutes home-routes
-  (GET "/" [] (home)))
--- webapp/delectus/src/delectus/views/layout.clj
+++
@@ -1,9 +0,0 @@
-(ns delectus.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
-
-(defn common [& body]
-  (html5
-    [:head
-     [:title "Delectus"]
-     (include-css "/css/screen.css")]
-    [:body body]))
