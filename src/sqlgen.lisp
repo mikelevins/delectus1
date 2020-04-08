948c17df48fdaf9dfd34c01c394b01b47eceabc3
mikel evins <mevins@me.com>
2020-04-08 16:27:08 -0500

    rebuilding delectus file operations

--- src/sqlgen.lisp
+++ src/sqlgen.lisp
@@ -11,6 +11,100 @@
 (in-package :delectus)
 (in-readtable :delectus)
 
+;;; =====================================================================
+;;; helper functions
+;;; =====================================================================
+
+(defun make-metadata-params (optype opid origin revision timestamp item name deleted peer)
+  (values (list "optype" "opid" "origin" "revision" "timestamp" "item" "name" "deleted" "peer")
+          (list optype opid origin revision timestamp item name deleted peer)))
+
+(defun make-column-params (column-data)
+  (values (mapcar (lambda (c)(fset:@ c :|id|)) column-data)
+          (mapcar #'to-json column-data)))
+
+(defun make-item-params (column-data vals)
+  (assert (= (length column-data)(length vals))()
+          "The number of values must equal the number of columns")
+  (values (mapcar (lambda (c)(fset:@ c :|id|)) column-data)
+          vals))
+
+;;; =====================================================================
+;;; SQL constructor functions
+;;; =====================================================================
+
+;;; ---------------------------------------------------------------------
+;;; sql-create-delectus-table
+;;; ---------------------------------------------------------------------
+
+(defun sql-create-delectus-table ()
+  (values
+   "CREATE TABLE `delectus` ( `id` TEXT, `origin` TEXT, `format` TEXT, `next_revision` INTEGER )"
+   nil))
+
+;;; (sql-create-delectus-table)
+
+;;; ---------------------------------------------------------------------
+;;; sql-populate-delectus-table
+;;; ---------------------------------------------------------------------
+
+(defun sql-populate-delectus-table (id origin format next-revision)
+  (values
+   "INSERT INTO `delectus` (`id`, `origin`, `format`, `next_revision`) VALUES (?, ?, ?, ?)"
+   (list id origin format next-revision)))
+
+;;; (sql-populate-delectus-table (delectus::makeid) delectus::*origin* delectus::+delectus-format-version+ 3)
+
+;;; ---------------------------------------------------------------------
+;;; sql-create-listdata-table
+;;; ---------------------------------------------------------------------
+
+(defun sql-create-listdata-table ()
+  (values
+   "CREATE TABLE `list_data` ( `optype` TEXT, `opid` TEXT, `origin` TEXT, `revision` INTEGER, `timestamp` TEXT, `item` TEXT, `name` TEXT, `deleted` TEXT, `peer` TEXT )"
+   nil))
+
+;;; (sql-create-listdata-table)
+
+;;; ---------------------------------------------------------------------
+;;; sql-add-userdata-column
+;;; ---------------------------------------------------------------------
+
+(defun sql-add-userdata-column (label type)
+  (values
+   (format nil "ALTER TABLE `list_data` ADD `~A` ~A"
+           label type)
+   nil))
+
+;;; (sql-add-userdata-column (makeid) "TEXT")
+
+;;; ---------------------------------------------------------------------
+;;; sql-list-id
+;;; ---------------------------------------------------------------------
+
+(defun sql-list-id ()
+  (values
+   "SELECT `id` FROM `delectus` LIMIT 1"
+   nil))
+
+;;; ---------------------------------------------------------------------
+;;; sql-list-origin
+;;; ---------------------------------------------------------------------
+
+(defun sql-list-origin ()
+  (values
+   "SELECT `origin` FROM `delectus` LIMIT 1"
+   nil))
+
+;;; ---------------------------------------------------------------------
+;;; sql-list-format
+;;; ---------------------------------------------------------------------
+
+(defun sql-list-format ()
+  (values
+   "SELECT `format` FROM `delectus` LIMIT 1"
+   nil))
+
 ;;; ---------------------------------------------------------------------
 ;;; sql-increment-next-revision
 ;;; ---------------------------------------------------------------------
@@ -68,3 +162,81 @@
   (values
    "SELECT * FROM `list_data` WHERE `optype`='sync' ORDER BY `revision` DESC, `origin` DESC LIMIT 1"
    nil))
+
+;;; ---------------------------------------------------------------------
+;;; sql-assert-listname
+;;; ---------------------------------------------------------------------
+
+(defun sql-assert-listname (opid origin revision timestamp item name deleted peer)
+  (bind ((optype "listname")
+         (params vals (make-metadata-params optype opid origin revision timestamp item name deleted peer))
+         (placeholders (mapcar (constantly "?") params))
+         (params-string (join-strings ", " params))
+         (placeholders-string (join-strings ", " placeholders))
+         (sql (format nil "INSERT INTO `list_data` (~A) VALUES (~A)"
+                      params-string placeholders-string)))
+    (values sql vals)))
+
+;;; (sql-assert-listname (makeid)(makeid) 3 (now-timestamp) nil "A List" nil nil)
+
+;;; ---------------------------------------------------------------------
+;;; sql-assert-columns
+;;; ---------------------------------------------------------------------
+
+(defun sql-assert-columns (opid origin revision timestamp item name deleted peer &key column-data)
+  (bind ((optype "columns")
+         (meta-params meta-vals (make-metadata-params optype opid origin revision timestamp item name deleted peer))
+         (column-params column-vals (make-column-params column-data))
+         (params (append meta-params column-params))
+         (vals (append meta-vals column-vals))
+         (placeholders (mapcar (constantly "?") params))
+         (params-string (join-strings ", " params))
+         (placeholders-string (join-strings ", " placeholders))
+         (sql (format nil "INSERT INTO `list_data` (~A) VALUES (~A)"
+                      params-string placeholders-string)))
+    (values sql vals)))
+
+;;; (setf $col0 {:|id| (makeid) :|name| "Column 0" :|type| :false :|order| 10.0 :|sort| "ASC" :|title| :true :|subtitle| :false :|deleted| :false})
+;;; (setf $col1 {:|id| (makeid) :|name| "Column 1" :|type| :false :|order| 20.0 :|sort| :false :|title| :false :|subtitle| :false :|deleted| :false})
+;;; (setf $col2 {:|id| (makeid) :|name| "Column 2" :|type| :false :|order| 30.0 :|sort| :false :|title| :false :|subtitle| :false :|deleted| :false})
+;;; (sql-assert-columns (makeid)(makeid) 3 (now-timestamp) nil nil nil nil :column-data (list $col0 $col1 $col2))
+
+
+;;; ---------------------------------------------------------------------
+;;; sql-assert-item
+;;; ---------------------------------------------------------------------
+
+(defun sql-assert-item (opid origin revision timestamp item name deleted peer &key column-data column-values)
+  (bind ((optype "item")
+         (meta-params meta-vals (make-metadata-params optype opid origin revision timestamp item name deleted peer))
+         (item-params item-vals (make-item-params column-data column-values))
+         (params (append meta-params item-params))
+         (vals (append meta-vals item-vals))
+         (placeholders (mapcar (constantly "?") params))
+         (params-string (join-strings ", " params))
+         (placeholders-string (join-strings ", " placeholders))
+         (sql (format nil "INSERT INTO `list_data` (~A) VALUES (~A)"
+                      params-string placeholders-string)))
+    (values sql vals)))
+
+;;; (setf $col0 {:|id| (makeid) :|name| "Column 0" :|type| :false :|order| 10.0 :|sort| "ASC" :|title| :true :|subtitle| :false :|deleted| :false})
+;;; (setf $col1 {:|id| (makeid) :|name| "Column 1" :|type| :false :|order| 20.0 :|sort| :false :|title| :false :|subtitle| :false :|deleted| :false})
+;;; (setf $col2 {:|id| (makeid) :|name| "Column 2" :|type| :false :|order| 30.0 :|sort| :false :|title| :false :|subtitle| :false :|deleted| :false})
+;;; (sql-assert-item (makeid)(makeid) 3 (now-timestamp) (makeid) nil nil nil :column-data (list $col0 $col1 $col2) :column-values [0 1 2])
+
+
+;;; ---------------------------------------------------------------------
+;;; sql-assert-sync
+;;; ---------------------------------------------------------------------
+
+(defun sql-assert-sync (opid origin revision timestamp item name deleted peer)
+  (bind ((optype "sync")
+         (params vals (make-metadata-params optype opid origin revision timestamp item name deleted peer))
+         (placeholders (mapcar (constantly "?") params))
+         (params-string (join-strings ", " params))
+         (placeholders-string (join-strings ", " placeholders))
+         (sql (format nil "INSERT INTO `list_data` (~A) VALUES (~A)"
+                      params-string placeholders-string)))
+    (values sql vals)))
+
+;;; (sql-assert-sync (makeid)(makeid) 3 (now-timestamp) nil nil nil (makeid))
--- src/store.lisp
+++ src/store.lisp
@@ -15,8 +15,8 @@
 ;;; ABOUT
 ;;; ---------------------------------------------------------------------
 ;;; functions whose names start with "db-" require a valid open
-;;; SQLITE-HANDLE; in other words, they operate on open SQLite
-;;; databases.
+;;; SQLITE-HANDLE; in other words, they operate within transactions on
+;;; open SQLite databases.
 ;;;
 ;;; the corresponding functions whose names omit the "db-" prefix
 ;;; operate on strings or pathnames, and automatically open and
@@ -33,8 +33,18 @@
 ;;; creating the list file
 ;;; ---------------------------------------------------------------------
 
-(defmethod db-create-delectus-table ((db sqlite-handle))
-  )
+(defmethod db-create-delectus-table ((db sqlite-handle) &key listid)
+  (let* ((listid (or listid (makeid)))
+         (listname-rev 0)
+         (columns-rev 1)
+         (item-rev 2)
+         (next-rev (1+ item-rev)))
+    ;; create the delectus table
+    (bind ((sql vals (sql-create-delectus-table)))
+      (apply 'execute-non-query db sql vals))
+    ;; populate it
+    (bind ((sql vals (sql-populate-delectus-table listid *origin* +delectus-format-version+ next-rev)))
+      (apply 'execute-non-query db sql vals))))
 
 (defmethod db-create-listdata-table ((db sqlite-handle))
   )
@@ -49,23 +59,75 @@
 (defmethod create-delectus-file ((list-name string)(db-path string))
   (create-delectus-file list-name (pathname db-path)))
 
+;;; (create-delectus-file "Test List" "/Users/mikel/Desktop/testlist.delectus2")
+
+;;; ---------------------------------------------------------------------
+;;; adding userdata columns
+;;; ---------------------------------------------------------------------
+
+(defmethod db-add-userdata-column ((db sqlite-handle) (column-id string))
+  (bind ((add-column-sql _ignore (sql-add-userdata-column column-id "TEXT")))
+    (apply 'execute-non-query db sql vals)))
+
+(defun add-userdata-column ((db-path pathname) (column-id string))
+  (with-open-database (db db-path)
+    (with-transaction db
+      (db-add-userdata-column db column-id))))
+
+(defun add-userdata-column ((db-path string) (column-id string))
+  (add-userdata-column (pathname db-path) column-id))
+
 ;;; ---------------------------------------------------------------------
 ;;; fetching data
 ;;; ---------------------------------------------------------------------
 
-;;; listid
+;;; list-id
 ;;; ---------------------------------------------------------------------
 
-(defmethod db-get-listid ((db sqlite-handle))
-  )
+(defmethod db-get-list-id ((db sqlite-handle))
+  (bind ((sqlget _ignore (sql-list-id)))
+    (execute-single db sqlget)))
 
-(defmethod get-listid ((db-path pathname))
+(defmethod get-list-id ((db-path pathname))
   (with-open-database (db db-path)
-    (with-transaction db
-      (db-get-listid db))))
+    (db-get-list-id db)))
 
-(defmethod get-listid ((path string))
-  (get-listid (pathname path)))
+(defmethod get-list-id ((path string))
+  (get-list-id (pathname path)))
+
+;;; (get-list-id "/Users/mikel/Desktop/testlist.delectus2")
+
+;;; list-origin
+;;; ---------------------------------------------------------------------
+
+(defmethod db-get-list-origin ((db sqlite-handle))
+  (bind ((sqlget _ignore (sql-list-origin)))
+    (execute-single db sqlget)))
+
+(defmethod get-list-origin ((db-path pathname))
+  (with-open-database (db db-path)
+    (db-get-list-origin db)))
+
+(defmethod get-list-origin ((path string))
+  (get-list-origin (pathname path)))
+
+;;; (get-list-origin "/Users/mikel/Desktop/testlist.delectus2")
+
+;;; list-format
+;;; ---------------------------------------------------------------------
+
+(defmethod db-get-list-format ((db sqlite-handle))
+  (bind ((sqlget _ignore (sql-list-format)))
+    (execute-single db sqlget)))
+
+(defmethod get-list-format ((db-path pathname))
+  (with-open-database (db db-path)
+    (db-get-list-format db)))
+
+(defmethod get-list-format ((path string))
+  (get-list-format (pathname path)))
+
+;;; (get-list-format "/Users/mikel/Desktop/testlist.delectus2")
 
 ;;; revision number
 ;;; ---------------------------------------------------------------------
@@ -96,13 +158,12 @@
 
 (defmethod get-latest-listname ((db-path pathname))
   (with-open-database (db db-path)
-    (with-transaction db
-      (db-get-latest-listname db))))
+    (db-get-latest-listname db)))
 
 (defmethod get-latest-listname ((db-path string))
   (get-latest-listname (pathname db-path)))
 
-;;; (get-latest-listname "/Users/mikel/Desktop/testlist.delectus2")
+;;; (time (get-latest-listname "/Users/mikel/Desktop/testlist.delectus2"))
 
 
 ;;; columns ops
@@ -114,13 +175,12 @@
 
 (defmethod get-latest-columns ((db-path pathname))
   (with-open-database (db db-path)
-    (with-transaction db
-      (db-get-latest-columns db))))
+    (db-get-latest-columns db)))
 
 (defmethod get-latest-columns ((db-path string))
   (get-latest-columns (pathname db-path)))
 
-;;; (get-latest-columns "/Users/mikel/Desktop/testlist.delectus2")
+;;; (time (get-latest-columns "/Users/mikel/Desktop/testlist.delectus2"))
 
 ;;; item ops
 ;;; ---------------------------------------------------------------------
@@ -137,13 +197,12 @@
 
 (defmethod get-latest-items ((db-path pathname))
   (with-open-database (db db-path)
-    (with-transaction db
-      (db-get-latest-items db))))
+    (db-get-latest-items db)))
 
 (defmethod get-latest-items ((db-path string))
   (get-latest-items (pathname db-path)))
 
-;;; (get-latest-items "/Users/mikel/Desktop/testlist.delectus2")
+;;; (time (get-latest-items "/Users/mikel/Desktop/testlist.delectus2"))
 
 
 ;;; sync ops
@@ -155,13 +214,14 @@
 
 (defmethod get-latest-sync ((db-path pathname))
   (with-open-database (db db-path)
-    (with-transaction db
-      (db-get-latest-sync db))))
+    (db-get-latest-sync db)))
 
 (defmethod get-latest-sync ((db-path string))
   (get-latest-sync (pathname db-path)))
 
 ;;; (get-latest-sync "/Users/mikel/Desktop/testlist.delectus2")
+
+
 
 ;;; ---------------------------------------------------------------------
 ;;; asserting ops
@@ -176,11 +236,19 @@
 
 (defmethod db-assert-listname ((db sqlite-handle)
                                &key opid (origin *origin*) revision timestamp name)
-  )
+  (let ((opid (or opid (makeid)))
+        (revision (or revision (db-get-next-revision db)))
+        (timestamp (or timestamp (now-timestamp))))
+    (bind ((sql vals
+                (sql-assert-listname opid origin revision timestamp nil name nil nil)))
+      (apply 'execute-non-query db sql vals))))
 
 (defmethod assert-listname ((db-path pathname)
                             &key opid (origin *origin*) revision timestamp name)
-  )
+  (assert (stringp name)() "The :NAME parameter must be a text string")
+  (with-open-database (db db-path)
+    (with-transaction db
+      (db-assert-listname db :opid opid :origin origin :revision revision :timestamp timestamp :name name))))
 
 (defmethod assert-listname ((db-path string)
                             &key opid (origin *origin*) revision timestamp name)
@@ -192,32 +260,51 @@
 
 
 (defmethod db-assert-columns ((db sqlite-handle)
-                              &key opid origin revision timestamp columns)
-  )
+                              &key opid origin revision timestamp column-data)
+  (let ((opid (or opid (makeid)))
+        (revision (or revision (db-get-next-revision db)))
+        (timestamp (or timestamp (now-timestamp))))
+    (bind ((sql vals
+                (sql-assert-columns opid origin revision timestamp nil nil nil nil :column-data column-data)))
+      (apply 'execute-non-query db sql vals))))
 
 (defmethod assert-columns ((db-path pathname)
                            &key opid origin revision timestamp columns)
-  )
+  (with-open-database (db db-path)
+    (with-transaction db
+      (db-assert-columns db :opid opid :origin origin :revision revision :timestamp timestamp :column-data column-data))))
 
 (defmethod assert-columns ((db-path string)
-                           &key opid origin revision timestamp columns)
-  )
+                           &key opid origin revision timestamp column-data)
+  (assert-columns (pathname db-path)
+                  :opid opid :origin origin :revision revision :timestamp timestamp :column-data column-data))
 
 ;;; item ops
 ;;; ---------------------------------------------------------------------
 
 
 (defmethod db-assert-item ((db sqlite-handle)
-                           &key opid origin revision timestamp item deleted columns)
-  )
+                           &key opid origin revision timestamp item deleted column-data column-values)
+  (let ((opid (or opid (makeid)))
+        (revision (or revision (db-get-next-revision db)))
+        (timestamp (or timestamp (now-timestamp))))
+    (bind ((sql vals
+                (sql-assert-item opid origin revision timestamp nil nil nil nil
+                                 :column-data column-data :column-values column-values)))
+      (apply 'execute-non-query db sql vals))))
 
 (defmethod assert-item ((db-path pathname)
-                        &key opid origin revision timestamp item deleted columns)
-  )
+                        &key opid origin revision timestamp item deleted column-data column-values)
+  (with-open-database (db db-path)
+    (with-transaction db
+      (db-assert-item db :opid opid :origin origin :revision revision :timestamp timestamp
+                      :column-data column-data :column-values column-values))))
 
 (defmethod assert-item ((db-path string)
-                        &key opid origin revision timestamp item deleted columns)
-  )
+                        &key opid origin revision timestamp item deleted column-data column-values)
+  (assert-item (pathname db-path)
+               :opid opid :origin origin :revision revision :timestamp timestamp
+               :column-data column-data :column-values column-values))
 
 
 ;;; sync ops
@@ -226,12 +313,21 @@
 
 (defmethod db-assert-sync ((db sqlite-handle)
                            &key opid origin revision timestamp peer)
-  )
+  (let ((opid (or opid (makeid)))
+        (revision (or revision (db-get-next-revision db)))
+        (timestamp (or timestamp (now-timestamp))))
+    (bind ((sql vals
+                (sql-assert-sync opid origin revision timestamp nil nil nil peer)))
+      (apply 'execute-non-query db sql vals))))
 
 (defmethod assert-sync ((db-path pathname)
                         &key opid origin revision timestamp peer)
-  )
+  (assert (stringp name)() "The :PEER parameter must be an identity")
+  (with-open-database (db db-path)
+    (with-transaction db
+      (db-assert-sync db :opid opid :origin origin :revision revision :timestamp timestamp :peer peer))))
 
 (defmethod assert-sync ((db-path string)
                         &key opid origin revision timestamp peer)
-  )
+  (assert-listname (pathname db-path)
+                   :opid opid :origin origin :revision revision :timestamp timestamp :peer peer))
