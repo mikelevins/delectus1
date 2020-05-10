67be09b156d57640853338894100e970aac3f4ca
mikel evins <mevins@me.com>
2020-05-10 18:12:58 -0500

    added code for recording and reading unique node ID

--- xsrc/system-node.lisp
+++ xsrc/system-node.lisp
@@ -11,3 +11,21 @@
 
 (in-package #:delectus)
 
+;;; TODO: rewrite this to use proper platform-specific mechanisms instead of hardcoding it
+(defun delectus-node-identity-path ()
+  (pathname "/Users/mikel/Library/Application Support/Delectus/nodeid"))
+
+(defun create-delectus-node-identity-file ()
+  (with-open-file (out (delectus-node-identity-path) :direction :output)
+    (format out "~A" (make-identity-string))))
+
+(let ((nodeid nil))
+  (defun delectus-node-identity ()
+    (or nodeid
+        (let ((idpath (probe-file (delectus-node-identity-path))))
+          (unless idpath
+            (create-delectus-node-identity-file))
+          (with-open-file (in idpath)
+            (setf nodeid
+                  (string->identity (symbol-name (read in)))))
+          nodeid))))
