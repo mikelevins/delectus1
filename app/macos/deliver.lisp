67bd24466503ad4704f502bbe41c00211026781d
mikel evins <mevins@me.com>
2020-05-19 09:27:41 -0500

    working on macos app delivery

--- app/macos/deliver.lisp
+++ app/macos/deliver.lisp
@@ -10,15 +10,18 @@
     (load quicklisp-init)))
 
 
-(load "/Users/mikel/Workshop/src/delectus/delectus.asd")
-(load "/Users/mikel/Workshop/src/delectus/delectus-macos.asd")
+;;(load "/Users/mikel/Workshop/src/delectus/delectus.asd")
+;;(load "/Users/mikel/Workshop/src/delectus/delectus-macos.asd")
 
 (defvar *project-root-path* "/Users/mikel/Workshop/src/delectus/")
-(defvar *target-application-path* "/Users/mikel/Workshop/src/delectus/delivery/macos/Delectus2")
-(defvar *bundle-template-path* "/Users/mikel/Workshop/src/delectus/delivery/macos/bundle-templates/Delectus2.app/")
+(defvar *target-application-path* "/Users/mikel/Workshop/src/delectus/app/macos/Delectus2")
+(defvar *bundle-template-path* "/Users/mikel/Workshop/src/delectus/app/macos/bundle-templates/Delectus2.app/")
 
-(asdf:load-system :delectus)
-(asdf:load-system :delectus-macos)
+;;(asdf:load-system :delectus)
+;;(asdf:load-system :delectus-macos)
+
+(ql:quickload :delectus)
+(ql:quickload :delectus-macos)
 
 (defun cocoa-application-interface-item-title (self prefix)
   (string-append prefix " " (capi:interface-title self)))
@@ -95,9 +98,9 @@
 (defun create-delectus-bundle (bundle-path)
   (let* ((created-path (create-macos-application-bundle bundle-path
                                                         :template-bundle (lw:pathname-location *bundle-template-path*)))
-         (sqlite-source-path (merge-pathnames "delivery/macos/lib/libsqlite3.dylib"
+         (sqlite-source-path (merge-pathnames "app/macos/lib/libsqlite3.dylib"
                                               *project-root-path*))
-         (sqlite-dest-path (merge-pathnames "delivery/macos/Delectus2.app/Contents/MacOS/libsqlite3.dylib"
+         (sqlite-dest-path (merge-pathnames "app/macos/Delectus2.app/Contents/MacOS/libsqlite3.dylib"
                             *project-root-path*)))
     (copy-file sqlite-source-path sqlite-dest-path)
     created-path))
---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf "/Users/mikel/Workshop/src/delectus/app/macos/Delectus2.app"
