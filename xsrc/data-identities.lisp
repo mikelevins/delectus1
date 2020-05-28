9a9fdac111aa7bb2a3790387631b74006c88707c
mikel evins <mevins@me.com>
2020-05-28 12:07:40 -0500

    added columns labels; updated identities to use base64 encoding

--- xsrc/data-identities.lisp
+++ xsrc/data-identities.lisp
@@ -51,50 +51,26 @@
 ;;; ---------------------------------------------------------------------
 ;;; identity strings
 ;;; ---------------------------------------------------------------------
-;;; an identity-string is an identity printed to a downcased
-;;; hexadecimal string.
+;;; an identity-string is a base64-encoded identity.
+
+(defparameter +delectus-identity-string-length+ 22)
 
 (defmethod identity->string ((id vector))
   (assert (identity id)() "Not a valid identity: ~S" id)
-  (string-downcase
-   (with-output-to-string (out)
-     (loop for b across id
-        do (format out "~2,'0x" b)))))
+  (subseq (string-downcase
+           (binascii:encode-base64 id))
+          0 +delectus-identity-string-length+))
 
 ;;; (identity->string (makeid))
-
-(defparameter +delectus-identity-string-length+
-  (length (identity->string (makeid))))
-
-(defmethod identity-string? (thing) nil)
-
-(defmethod identity-string? ((thing string))
-  (and (= 32 (length thing))
-       (let ((result t))
-         (block checking
-           (loop for i from 0 below (length thing)
-              do (unless (find (elt thing i) "0123456789abcdefABCDEF")
-                   (setf result nil)
-                   (return-from checking nil))))
-         result)))
 
 (defun make-identity-string ()
   (identity->string (makeid)))
 
 ;;; (time (make-identity-string))
-;;; (identity-string? (make-identity-string))
 
 (defmethod string->identity ((identity string))
-  (assert (identity-string? identity)() "Not a valid identity-string: ~S" identity)
-  (let* ((uuid-string (join-strings "-"
-                                    (list (subseq identity 0 8)
-                                          (subseq identity 8 12)
-                                          (subseq identity 12 16)
-                                          (subseq identity 16 20)
-                                          (subseq identity 20))))
-         (uuid (uuid:make-uuid-from-string uuid-string)))
-    (coerce (uuid:uuid-to-byte-array uuid)
-            '(simple-vector 16))))
+  (coerce (binascii:decode-base64 identity)
+          '(simple-vector 16)))
 
 ;;; (string->identity (identity->string (makeid)))
 ;;; (string->identity (make-identity-string))
