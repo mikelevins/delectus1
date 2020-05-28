c289773cd12690e2b565ca9740b4da2a57a758f3
mikel evins <mevins@me.com>
2020-05-28 08:44:40 -0500

    working on a new file format; updated editlog doc; created xsrc to work on implementation of the documented editlog format; bumped the Delectus version and file-format version

---
+++ xsrc/system-syntax.lisp
@@ -0,0 +1,49 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          system-syntax.lisp
+;;;; Project:       delectus 2
+;;;; Purpose:       syntax extensions
+;;;; Author:        mikel evins
+;;;; Copyright:     2015 by mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(in-package :delectus)
+
+
+(set-syntax-from-char #\{ #\()
+(set-syntax-from-char #\} #\))
+
+(set-macro-character #\{
+                     (lambda (stream char)
+                       (declare (ignore char))
+                       (let ((elts (read-delimited-list #\} stream t)))
+                         `(convert 'wb-map (loop for tail on (cl:list ,@elts) by #'cddr
+                                              collect (cons (first tail)
+                                                            (second tail)))))))
+
+
+(set-macro-character #\[
+                     (lambda (stream char)
+                       (declare (ignore char))
+                       (let ((elts (read-delimited-list #\] stream t)))
+                         ` (cl:list ,@elts))))
+
+(set-macro-character #\] (get-macro-character #\)))
+
+
+(in-package :fset)
+
+;;; change to a less visually-cluttered printing style for wb-maps
+
+(defun print-wb-map (map stream level)
+  (declare (ignore level))
+  (pprint-logical-block (stream nil :prefix "{")
+    (do-map (x y map)
+      (pprint-pop)
+      (write-char #\Space stream)
+      (pprint-newline :linear stream)
+      (write x :stream stream)
+      (write-char #\Space stream)
+      (write y :stream stream))
+    (format stream " }~:[~;/~:*~S~]" (map-default map))))
