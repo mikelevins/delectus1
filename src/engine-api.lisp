ba2acdca186ef947d20ad644ae3b183d1bbfb001
mikel evins <mevins@me.com>
2020-06-03 18:45:36 -0500

    adding test code and comments to engine-api

--- src/engine-api.lisp
+++ src/engine-api.lisp
@@ -11,7 +11,7 @@
 (in-package #:delectus)
 
 ;;; ---------------------------------------------------------------------
-;;; getting items data
+;;; creating the temporary "latest_items" table
 ;;; ---------------------------------------------------------------------
 
 (defmethod db-check-latest-items-table-exists ((db sqlite-handle))
@@ -22,6 +22,10 @@
 (defmethod db-create-latest-items-table ((db sqlite-handle))
   (bind ((sql vals (sqlgen-create-latest-items-table)))
     (apply 'execute-to-list db sql vals)))
+
+;;; ---------------------------------------------------------------------
+;;; counting the latest items
+;;; ---------------------------------------------------------------------
 
 (defmethod db-count-latest-items ((db sqlite-handle))
   (unless (db-check-latest-items-table-exists db)
@@ -34,11 +38,9 @@
   (with-open-database (db dbpath)
     (db-count-latest-items db)))
 
-;;; (setf $movies-test-path (path "~/Desktop/Movies.delectus2"))
-;;; (time (count-latest-items $movies-test-path))
-
-;;; (setf $zips-test-path (path "~/Desktop/Zipcodes.delectus2"))
-;;; (time (count-latest-items $zips-test-path))
+;;; ---------------------------------------------------------------------
+;;; fetching the latest items
+;;; ---------------------------------------------------------------------
 
 (defmethod db-get-latest-items ((db sqlite-handle)
                                 &key
@@ -57,8 +59,27 @@
   (with-open-database (db dbpath)
     (db-get-latest-items db  :offset offset :limit limit)))
 
-;;; (setf $movies-test-path (path "~/Desktop/Movies.delectus2"))
-;;; (time (get-latest-items $movies-test-path))
 
+;;; ---------------------------------------------------------------------
+;;; tests
+;;; ---------------------------------------------------------------------
+
+;;; (setf $movies-test-path (path "~/Desktop/Movies.delectus2"))
 ;;; (setf $zips-test-path (path "~/Desktop/Zipcodes.delectus2"))
-;;; (time (get-latest-items $zips-test-path :offset 40000))
+;;; (setf $wordtest100-path (path "~/Desktop/wordtest100.delectus2"))
+;;; (setf $wordtest1k-path (path "~/Desktop/wordtest1k.delectus2"))
+;;; (setf $wordtest10k-path (path "~/Desktop/wordtest10k.delectus2"))
+;;; (setf $wordtest100k-path (path "~/Desktop/wordtest100k.delectus2"))
+
+;;; (time (count-latest-items $movies-test-path))
+;;; (time (count-latest-items $zips-test-path))
+;;; (time (count-latest-items $wordtest100-path))
+;;; (time (count-latest-items $wordtest1k-path))
+;;; (time (count-latest-items $wordtest10k-path))
+;;; (time (count-latest-items $wordtest100k-path))
+
+;;; (time (get-latest-items $movies-test-path))
+;;; (time (get-latest-items $zips-test-path))
+;;; (time (get-latest-items $wordtest100-path))
+;;; (time (get-latest-items $wordtest10k-path))
+;;; (time (get-latest-items $wordtest100k-path))
