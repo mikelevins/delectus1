83c4ebd0bd8b0a87c9ac1f805348f372fed2683b
mikel evins <mevins@me.com>
2020-06-09 11:17:23 -0500

    working on presentation

--- src/macos/view-item-card.lisp
+++ src/macos/view-item-card.lisp
@@ -77,7 +77,7 @@
   (:layouts
    (main-layout grid-layout '()
                 :columns 2
-                :reader main-layout :border 4))
+                :reader main-layout))
   
   ;; -- defaults ---------------------------------------------
   (:default-initargs :layout 'main-layout
@@ -85,6 +85,26 @@
 
 (defmethod value->presentation (value)(format nil "~A" value))
 (defmethod value->presentation ((value null)) "")
+
+(defun make-item-label-pane (label)
+  (make-instance 'title-pane
+                 :text label
+                 :font
+                 (gp:make-font-description
+                  :family "Helvetica" 
+                  :size 14
+                  :weight :bold                         
+                  :slant :roman)))
+
+(defun make-item-value-pane (value)
+  (make-instance 'title-pane
+                 :text value
+                 :font
+                 (gp:make-font-description
+                  :family "Helvetica" 
+                  :size 14
+                  :weight :normal                         
+                  :slant :roman)))
 
 (defmethod initialize-instance :after ((card item-card) &rest initargs 
                                        &key
@@ -98,9 +118,11 @@
                                     (item-data card))))
 
 (defun make-item-card-description (columns-data item-data)
-  (let ((lbls (mapcar #'delectus::column-description-name columns-data))
-        (vals (mapcar #'value->presentation item-data)))
-    (delectus::interleave lbls vals)))
+  (let* ((lbls (mapcar #'delectus::column-description-name columns-data))
+         (label-panes (mapcar #'make-item-label-pane lbls))
+         (vals (mapcar #'value->presentation item-data))
+         (value-panes (mapcar #'make-item-value-pane vals)))
+    (delectus::interleave label-panes value-panes)))
 
 ;;; (setf $movies (delectus::path "~/Desktop/Movies.delectus2"))
 ;;; (setf $items (delectus::get-latest-items $movies))
@@ -108,3 +130,5 @@
 ;;; (setf $it (elt $items 0))
 
 ;;; (setf $win (contain (make-instance 'item-card :columns-data $cols :item-data $it)))
+
+
