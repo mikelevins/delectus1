a4a622d0b9eb6394b2ed97d4ca44d42acb328625
mikel evins <mevins@me.com>
2020-06-22 11:00:40 -0500

    added --ignore to electron-packager so we omit dev-time files from the delivered Electron app

--- src/electron/buildapp.sh
+++ src/electron/buildapp.sh
@@ -4,4 +4,4 @@ sbcl --no-userinit --load lisp/Delectus2.asd --eval "(cl-user::buildapp)"
 
 # Package
 npm i
-electron-packager . $APP_NAME --overwrite --icon=./../../assets/images/app.icns 
+electron-packager . $APP_NAME --overwrite --icon=./../../assets/images/app.icns --ignore="\.gitignore|.*\.sh|lisp|README\.adoc"
