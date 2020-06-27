5c642643be43320c381381193bf2ce7350fe96f4
mikel evins <mevins@me.com>
2020-06-27 10:42:42 -0500

    translating Objective-C DelectusPrintView to Swift PrintView

---
+++ Delectus/PrintView.swift
@@ -0,0 +1,25 @@
+//
+//  PrintView.swift
+//  Delectus
+//
+//  Created by mikel evins on 6/27/20.
+//
+
+import Foundation
+import Cocoa
+
+@objc class PrintView : NSTextView {
+    var dataSource: DelectusDataSource?
+    var documentName: NSString = ""
+
+    init(frame aRect: NSRect, dataSource: DelectusDataSource, documentName: NSString) {
+        self.documentName = documentName
+        super.init(frame: aRect)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.dataSource = nil
+        self.documentName = ""
+        super.init(coder: aDecoder)
+    }
+}
