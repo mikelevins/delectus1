124e09e6239414cb377f59dc1bcf69bbadc1f955
mikel evins <mevins@me.com>
2020-06-29 16:08:01 -0500

    adding outlets to the printreview; tinkering with layout constraints

--- Delectus/PrintPreviewDocument.swift
+++ Delectus/PrintPreviewDocument.swift
@@ -9,5 +9,11 @@ import Foundation
 import Cocoa
 
 @objc class PrintPreviewDocument : NSDocument, NSTableViewDelegate {
-    
+    @IBOutlet weak var documentWindow: NSWindow!
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var chooseColumnsLabel: NSTextField!
+    @IBOutlet weak var showDeletedLabel: NSTextField!
+    @IBOutlet weak var tableScrollView: NSScrollView!
+    @IBOutlet weak var itemCountField: NSTextField!
+    @IBOutlet weak var filterField: NSSearchField!
 }
