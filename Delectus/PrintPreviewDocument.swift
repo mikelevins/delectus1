15749037356e41c8674c87559c9dcf8bb54438da
mikel evins <mevins@me.com>
2020-06-29 10:08:49 -0500

    added PrintPreviewDocument; working on improved printing

---
+++ Delectus/PrintPreviewDocument.swift
@@ -0,0 +1,13 @@
+//
+//  PrintPreviewDocument.swift
+//  Delectus
+//
+//  Created by mikel evins on 6/29/20.
+//
+
+import Foundation
+import Cocoa
+
+@objc class PrintPreviewDocument : NSDocument, NSTableViewDelegate {
+    
+}
